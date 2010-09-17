fa2257272f7a8b813e09e7f9bd9047b7b11d439b
Florian Hanke <florian.hanke@gmail.com>
2010-09-17 21:48:56 +0200

    + generators

--- server/lib/picky/generator.rb
+++ server/lib/picky/generator.rb
@@ -1,5 +1,7 @@
 module Picky
   
+  class NoGeneratorException < Exception; end
+  
   # This is a very simple project generator.
   # Not at all like Padrino's or Rails'.
   # (No diss, just by way of a faster explanation)
@@ -20,27 +22,33 @@ module Picky
     #
     # This will "route" the commands to the right specific generator.
     #
-    def run args
-      type = args.shift
-      generator_class = types[type.to_sym]
-      
-      # TODO Explain why not here.
-      return unless generator_class
-      
-      generator = generator_class.new *args
-      generator.run
+    def generate args
+      generator = generator_for *args
+      generator.generate
+    end
+    
+    #
+    #
+    def generator_for identifier, *args
+      generator_class = types[identifier.to_sym]
+      raise NoGeneratorException unless generator_class
+      generator_class.new *args
     end
     
     class Project
       
-      attr_reader :name
+      attr_reader :name, :prototype_project_basedir
       
       def initialize name, *args
         @name = name
+        @prototype_project_basedir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'prototype_project'))
       end
       
-      def run
-        p "Doing something with #{name}"
+      #
+      #
+      def generate
+        target = File.expand_path File.join(Dir.pwd, name)
+        FileUtils.cp prototype_project_basedir, target, :verbose => true
       end
       
     end
--- server/spec/lib/generator_spec.rb
+++ server/spec/lib/generator_spec.rb
@@ -3,17 +3,33 @@ require 'spec_helper'
 
 describe Picky::Generator do
   
-  before(:each) do
-    @generator = Picky::Generator.new
+  describe "main class" do
+    before(:each) do
+      @generator = Picky::Generator.new
+    end
+
+    describe "generate" do
+      it "should exist" do
+        lambda { @generator.generate(['project', 'some_project']) }.should_not raise_error
+      end
+      it "should " do
+
+      end
+    end
   end
   
-  describe "run" do
-    it "should exist" do
-      lambda { @generator.run(['some_arg', 'some_other_arg']) }.should_not raise_error
+  describe Picky::Generator::Project do
+    
+    before(:each) do
+      @generator = Picky::Generator::Project.new :some_name, :some_args
     end
-    it "should " do
-      
+    
+    describe "generate" do
+      it "should " do
+        @generator.generate
+      end
     end
+    
   end
   
 end
