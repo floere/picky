9bdf3b360ac31d5804d3f5363567e154cf04b472
Florian Hanke <florian.hanke@gmail.com>
2010-09-28 18:16:08 +0200

    + set the test project spec helper settings correctly

--- server/test_project/spec/spec_helper.rb
+++ server/test_project/spec/spec_helper.rb
@@ -6,4 +6,16 @@ require 'rubygems'
 require File.expand_path(File.dirname(__FILE__) + "/../../lib/picky")
 require 'spec'
 
+
+
+# Set some spec preconditions.
+#
+SEARCH_ROOT = File.join SEARCH_ROOT, 'test_project'
+puts "Redefined SEARCH_ROOT to '#{SEARCH_ROOT}' for the tests."
+
+SearchLog = Loggers::Search.new ::Logger.new(STDOUT)
+puts "Using STDOUT as test log."
+
+
+
 Loader.load_application
