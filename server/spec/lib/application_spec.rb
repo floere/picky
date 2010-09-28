32d94f98b8b7b3565119b2f35068c09ee5458d4f
Florian Hanke <florian.hanke@gmail.com>
2010-09-28 18:44:55 +0200

    + application specs

--- server/spec/lib/application_spec.rb
+++ server/spec/lib/application_spec.rb
@@ -4,10 +4,22 @@ require 'spec_helper'
 
 describe Application do
   
+  describe 'routing' do
+    it 'should be there' do
+      lambda { Application.routing }.should_not raise_error
+    end
+    it "should return a new Routing instance" do
+      Application.routing.should be_kind_of(Routing)
+    end
+    it "should cache the instance" do
+      Application.routing.should == Application.routing
+    end
+  end
+  
   describe 'call' do
     before(:each) do
       @routes = stub :routes
-      Application.stub! :routes => @routes
+      Application.stub! :routing => @routes
     end
     it 'should delegate' do
       @routes.should_receive(:call).once.with :env
@@ -16,55 +28,33 @@ describe Application do
     end
   end
   
-  describe 'reset_routes' do
-    it 'should return a new Routing instance' do
-      Application.reset_routes.should be_kind_of(Routing)
-    end
+  describe "indexes" do
+    
   end
-  
-  describe 'routes' do
-    before(:each) do
-      Application.instance_variable_set :@routes, nil
-    end
-    it 'should generate a new routes object if one has not been generated' do
-      Application.should_receive(:reset_routes).once
-      
-      Application.routes
-    end
-    it 'should not generate a new routes object if it has been called before' do
-      Application.routes
-      
-      Application.should_receive(:reset_routes).never
-      
-      Application.routes
-    end
-  end
-  
-  describe 'routing' do
+  describe "indexes_configuration" do
     it 'should be there' do
-      lambda { Application.routing do end }.should_not raise_error
+      lambda { Application.indexes_configuration }.should_not raise_error
     end
-    it 'needs a block' do
-      lambda { Application.routing }.should raise_error(ArgumentError)
+    it "should return a new Routing instance" do
+      Application.indexes_configuration.should be_kind_of(Configuration::Indexes)
     end
-    context 'with routing' do
-      before(:each) do
-        Application.reset_routes
-      end
-      it 'should set defaults' do
-        Application.routes.should_receive(:defaults).once.with :a => :b
-        
-        Application.routing :a => :b do
-          
-        end
-      end
-      it 'should instance eval on the routing' do
-        Application.routes.should_receive(:flarb).once.with :glorb
-        
-        Application.routing do
-          flarb :glorb
-        end
-      end
+    it "should cache the instance" do
+      Application.indexes_configuration.should == Application.indexes_configuration
+    end
+  end
+  
+  describe "queries" do
+    
+  end
+  describe "queries_configuration" do
+    it 'should be there' do
+      lambda { Application.queries_configuration }.should_not raise_error
+    end
+    it "should return a new Routing instance" do
+      Application.queries_configuration.should be_kind_of(Configuration::Queries)
+    end
+    it "should cache the instance" do
+      Application.queries_configuration.should == Application.queries_configuration
     end
   end
   
