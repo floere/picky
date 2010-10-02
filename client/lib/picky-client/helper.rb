92264f0e2e8aef85b3c3a2fdb1b20af1d3a52c20
Florian Hanke <florian.hanke@gmail.com>
2010-10-02 04:21:07 +0200

    + interface helpers

--- client/lib/picky-client/helper.rb
+++ client/lib/picky-client/helper.rb
@@ -4,29 +4,55 @@ module Picky
   #
   class Helper
     
-    # TODO I18n stuff!
-    #
-    @@interface_html = <<-HTML
+    @@localized_interface = lambda { |options|
+      search_button_text = options[:button]     || 'search'
+      no_results         = options[:no_results] || 'Sorry, no results found!'
+      more_allocations   = options[:more]       || 'more'
+<<-HTML
 <div id="picky">
   <div class="dashboard empty">
     <div class="feedback">
-      <div title="# results" class="status"></div>
-      <input type="text" class="query" autocorrect="off">
-      <div title="clear" class="reset"></div>
+      <div class="status"></div>
+      <input type="text" autocorrect="off" class="query"/>
+      <div class="reset" title="clear"></div>
     </div>
-    <input type="button" value="search" class="search_button">
+    <input type="button" class="search_button" value="#{search_button_text}">
+  </div>
+  <ol class="results"></ol>
+  <div class="no_results">#{no_results}</div>
+  <div class="allocations">
+    <ol class="shown"></ol>
+    <ol class="more">#{more_allocations}</ol>
+    <ol class="hidden"></ol>
   </div>
 </div>
-    HTML
-    @@interface_html.gsub!(/[\n]/, '').squeeze!(' ')
-    @@interface_html.freeze
+HTML
+    }
+    
+    # Returns a standard search interface for easy starting.
     #
-    # Returns
+    # ... aka scaffolding ;)
     #
-    def self.interface
-      @@interface_html
+    # Options:
+    #  * button: The search button text.
+    #  * no_results: The text shown when there are no results.
+    #  * more: The text shown when there are more than X results.
+    #
+    # Usage, in Views:
+    #
+    #   = Picky::Helper.interface :button => 'Go go go!'
+    #
+    #
+    def self.interface options = {}
+      @@localized_interface[options].gsub!(/[\n]/, '').squeeze! ' '
+    end
+    
+    # Returns a cached version if you always use a single language.
+    #
+    def self.cached_interface options = {}
+      @interface ||= interface.freeze
     end
     
   end
-
+  
 end
--- client/spec/picky-client/helper_spec.rb
+++ client/spec/picky-client/helper_spec.rb
@@ -2,9 +2,24 @@ require 'spec_helper'
 
 describe Picky::Helper do
   
+  describe "cached_interface" do
+    it "should return good html" do
+      Picky::Helper.cached_interface.should == Picky::Helper.interface
+    end
+    it "should return the cached interface" do
+      Picky::Helper.cached_interface.object_id.should == Picky::Helper.cached_interface.object_id
+    end
+    it "should be frozen" do
+      Picky::Helper.cached_interface.should be_frozen
+    end
+  end
+  
   describe "interface" do
     it "should return good html" do
-      Picky::Helper.interface.should == "<div id=\"picky\"> <div class=\"dashboard empty\"> <div class=\"feedback\"> <div title=\"# results\" class=\"status\"></div> <input type=\"text\" class=\"query\" autocorrect=\"off\"> <div title=\"clear\" class=\"reset\"></div> </div> <input type=\"button\" value=\"search\" class=\"search_button\"> </div></div>"
+      Picky::Helper.interface.should == "<div id=\"picky\"> <div class=\"dashboard empty\"> <div class=\"feedback\"> <div class=\"status\"></div> <input type=\"text\" autocorrect=\"off\" class=\"query\"/> <div class=\"reset\" title=\"clear\"></div> </div> <input type=\"button\" class=\"search_button\" value=\"search\"> </div> <ol class=\"results\"></ol> <div class=\"no_results\">Sorry, no results found!</div> <div class=\"allocations\"> <ol class=\"shown\"></ol> <ol class=\"more\">more</ol> <ol class=\"hidden\"></ol> </div></div>"
+    end
+    it "should return good html" do
+      Picky::Helper.interface(:button => 'find', :no_results => 'SORRY!', :more => 'Click for more!').should == "<div id=\"picky\"> <div class=\"dashboard empty\"> <div class=\"feedback\"> <div class=\"status\"></div> <input type=\"text\" autocorrect=\"off\" class=\"query\"/> <div class=\"reset\" title=\"clear\"></div> </div> <input type=\"button\" class=\"search_button\" value=\"find\"> </div> <ol class=\"results\"></ol> <div class=\"no_results\">SORRY!</div> <div class=\"allocations\"> <ol class=\"shown\"></ol> <ol class=\"more\">Click for more!</ol> <ol class=\"hidden\"></ol> </div></div>"
     end
   end
   
