6fc62f9704ebc62fe0809cb3968a68cea92de412
Florian Hanke <florian.hanke@gmail.com>
2010-10-04 22:11:28 +0200

    + prototype project now much simpler

--- server/prototype_project/app/application.rb
+++ server/prototype_project/app/application.rb
@@ -8,21 +8,21 @@ class PickySearch < Application # The App Constant needs to be identical in appl
   
   # This is an example with books that you can adapt.
   #
-  # Note: Much more is possible, but let's start out easy.
+  # Note: Much more is possible, but let's start out super easy.
   #
-  # Ask me if you have questions!
+  # Ask me if you have questions or specific requests!
   #
   
   indexes do
-    illegal_characters(/[^äöüa-zA-Z0-9\s\/\-\"\&\.]/)
-    stopwords(/\b(und|der|die|das|mit|im|ein|des|dem|the|of)\b/)
+    illegal_characters(/[^a-zA-Z0-9\s\/\-\"\&\.]/)
+    stopwords(/\b(and|the|of|it|in|for)\b/)
     split_text_on(/[\s\/\-\"\&\.]/)
       
-    type :books,
-         Sources::DB.new('SELECT id, title, author, isbn13 as isbn FROM books', :file => 'app/db.yml'),
-         field(:title,  :qualifiers => [:t, :title, :titre], :similarity => Similarity::DoubleLevenshtone.new(3)), # Up to three similar title word indexed.
-         field(:author, :qualifiers => [:s, :author, :auteur]),
-         field(:isbn,   :qualifiers => [:i, :isbn],          :partial => Partial::None.new) # Partially searching on an ISBN makes not much sense.
+    add_index :books,
+              Sources::DB.new('SELECT id, title, author, isbn13 as isbn FROM books', :file => 'app/db.yml'),
+              field(:title, :similarity => Similarity::DoubleLevenshtone.new(3)), # Up to three similar title word indexed.
+              field(:author),
+              field(:isbn,  :partial => Partial::None.new) # Partially searching on an ISBN makes not much sense.
   end
   
   queries do
@@ -30,16 +30,12 @@ class PickySearch < Application # The App Constant needs to be identical in appl
     # Note that Picky needs the following characters to
     # pass through, as they are control characters: *"~:
     #
-    illegal_characters(/[^a-zA-Z0-9\s\/\-\,\&äöü\"\~\*\:]/)
-    stopwords(/\b(und|der|die|das|mit|ein|des|dem|the|of)\b/)
+    illegal_characters(/[^a-zA-Z0-9\s\/\-\,\&\"\~\*\:]/)
+    stopwords(/\b(and|the|of|it|in|for)\b/)
     split_text_on(/[\s\/\-\,\&]+/)
     
-    # Set some weights according to the position. Note that the order is important.
-    #
-    options = { :weights => Query::Weights.new([:title] => 6, [:author, :title] => 3) }
-    
-    route %r{^/books/full}, Query::Full.new(Indexes[:books], options)
-    route %r{^/books/live}, Query::Live.new(Indexes[:books], options)
+    route %r{^/books/full}, Query::Full.new(Indexes[:books])
+    route %r{^/books/live}, Query::Live.new(Indexes[:books])
     
     root 200
   end
