56a4d77e0f6ae5b85144fe1208edc48afe85b610
Florian Hanke <florian.hanke@gmail.com>
2010-10-11 01:28:41 +0200

    + scraping for speed

--- server/lib/picky/extensions/symbol.rb
+++ server/lib/picky/extensions/symbol.rb
@@ -2,18 +2,32 @@
 #
 class Symbol
   
-  # :keys.subtokens    # => [:key, :ke, :k]
-  # :keys.subtokens(2) # => [:key, :ke]
+  # :keys.subtokens    # => [:keys, :key, :ke, :k]
+  # :keys.subtokens(2) # => [:keys, :key, :ke]
   #
   def subtokens down_to_length = 1
-    sub, result = self.id2name, [self]
+    sub = self.id2name
     
     size = sub.size
     down_to_length = size + down_to_length if down_to_length < 0
     down_to_length = size if size < down_to_length
     
-    size.downto(down_to_length + 1) { result << sub.chop!.to_sym }
+    result = [self]
+    size.downto(down_to_length + 1) { result << sub.chop!.intern }
     result
+  end
+  
+  # TODO Duplicate code.
+  #
+  def each_subtoken down_to_length = 1
+    sub = self.id2name
+    
+    size = sub.size
+    down_to_length = size + down_to_length if down_to_length < 0
+    down_to_length = size if size < down_to_length
+    
+    yield self
+    size.downto(down_to_length + 1) { yield sub.chop!.intern }
   end
   
 end
--- server/spec/lib/extensions/symbol_spec.rb
+++ server/spec/lib/extensions/symbol_spec.rb
@@ -5,21 +5,116 @@ describe Symbol do
   context 'performance' do
     include Helpers::Measuring
     before(:each) do
+      @token = (((0..9).to_a)*10).to_s.to_sym
       GC.disable
     end
     after(:each) do
       GC.enable
       GC.start
     end
+    # Note: They influence each other. each_subtoken is faster though.
+    #
     it 'should be fast' do
-      s = (((0..9).to_a)*10).to_s.to_sym
-      
       timed do
-        s.subtokens
-      end.should < 0.001
+        @token.subtokens
+      end.should < 0.0009
+    end
+    it "should be fast" do
+      timed do
+        @token.each_subtoken do |subtoken| end
+      end.should < 0.0004
     end
   end
-
+  
+  describe "each_subtoken" do
+    context 'normal symbol' do
+      before(:each) do
+        @sym = :reinke
+      end
+      context 'no downto' do
+        it "should return an array of pieces of the original token, each 1 smaller than the other" do
+          result = []
+          @sym.each_subtoken do |subtoken|
+            result << subtoken
+          end
+          result.should == [:reinke, :reink, :rein, :rei, :re, :r]
+        end
+      end
+      context 'downto is larger than the symbol' do
+        before(:each) do
+          @downto = 8
+        end
+        it "should return an array of pieces of the original token, each 1 smaller than the other" do
+          result = []
+          @sym.each_subtoken(@downto) do |subtoken|
+            result << subtoken
+          end
+          result.should == [:reinke]
+        end
+      end
+      context 'downto is exactly the same as symbol' do
+        before(:each) do
+          @downto = 6
+        end
+        it "should return an array of pieces of the original token, each 1 smaller than the other" do
+          result = []
+          @sym.each_subtoken(@downto) do |subtoken|
+            result << subtoken
+          end
+          result.should == [:reinke]
+        end
+      end
+      context 'downto is smaller than the length of the symbol' do
+        before(:each) do
+          @downto = 4
+        end
+        it "should return an array of pieces of the original token, each 1 smaller than the other" do
+          result = []
+          @sym.each_subtoken(@downto) do |subtoken|
+            result << subtoken
+          end
+          result.should == [:reinke, :reink, :rein]
+        end
+      end
+      context 'downto is 1' do
+        before(:each) do
+          @downto = 1
+        end
+        it "should return an array of pieces of the original token, each 1 smaller than the other" do
+          result = []
+          @sym.each_subtoken(@downto) do |subtoken|
+            result << subtoken
+          end
+          result.should == [:reinke, :reink, :rein, :rei, :re, :r]
+        end
+      end
+      context 'downto is 0' do
+        before(:each) do
+          @downto = 0
+        end
+        it "should return an array of pieces of the original token, each 1 smaller than the other" do
+          result = []
+          @sym.each_subtoken(@downto) do |subtoken|
+            result << subtoken
+          end
+          result.should == [:reinke, :reink, :rein, :rei, :re, :r, :'']
+        end
+      end
+      context 'downto is less than zero' do
+        before(:each) do
+          @downto = -2
+        end
+        it "should return an array of pieces of the original token, each 1 smaller than the other" do
+          result = []
+          @sym.each_subtoken(@downto) do |subtoken|
+            result << subtoken
+          end
+          result.should == [:reinke, :reink, :rein]
+        end
+      end
+    end
+  end
+  
   describe "subtokens" do
     context 'normal symbol' do
       before(:each) do
