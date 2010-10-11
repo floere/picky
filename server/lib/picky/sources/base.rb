bff1fd16a925cb4aaccec3ba5a86d33885a51786
Florian Hanke <florian.hanke@gmail.com>
2010-10-11 02:47:11 +0200

    + doc

--- server/lib/picky/sources/base.rb
+++ server/lib/picky/sources/base.rb
@@ -1,16 +1,28 @@
 module Sources
   
+  # Sources are where your data comes from.
+  # Harvest is the most important method as it is used always to get data.
+  #
   class Base
     
+    # Note: Methods listed for illustrative purposes.
+    #
+    
+    # Yield the data (id, text for id) for the given type and field.
+    #
+    def harvest type, field
+      # yields nothing
+    end
+    
+    # Connects to the backend.
+    #
     def connect_backend
       
     end
     
+    # Take a snapshot of your data, if it is fast changing.
+    #
     def take_snapshot type
-      
-    end
-    
-    def harvest type, field
       
     end
     
