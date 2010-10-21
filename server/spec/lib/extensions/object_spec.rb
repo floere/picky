c65a460969cd21c6ee3098786511b489b0c8b3c5
Florian Hanke <florian.hanke@gmail.com>
2010-10-22 00:55:42 +0200

    + object specs

--- server/spec/lib/extensions/object_spec.rb
+++ server/spec/lib/extensions/object_spec.rb
@@ -2,8 +2,25 @@ require 'spec_helper'
 
 describe Object do
   
-  it "should fail" do
-    raise
+  before(:each) do
+    @object = Object.new
+  end
+  
+  describe "exclaim" do
+    it "delegates to puts" do
+      @object.should_receive(:puts).once.with :bla
+      
+      @object.exclaim :bla
+    end
+  end
+  
+  describe "timed_exclaim" do
+    it "should exclaim right" do
+      Time.stub! :now => Time.parse('07-03-1977 12:34:56')
+      @object.should_receive(:exclaim).once.with "12:34:56: bla"
+      
+      @object.timed_exclaim 'bla'
+    end
   end
   
 end
