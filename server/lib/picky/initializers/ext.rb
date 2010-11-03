1aaaed818a9d9a995e9dd78689439dc2a968d478
Florian Hanke <florian.hanke@gmail.com>
2010-11-03 20:17:48 +0100

    + compile c code if the bundle is not there

--- server/lib/picky/initializers/ext.rb
+++ server/lib/picky/initializers/ext.rb
@@ -1,1 +1,11 @@
-require File.expand_path '../../ext/ruby19/performant', __FILE__
+# TODO Move to ext. Call possibly_compile.rb.
+#
+begin
+  require File.expand_path '../../ext/ruby19/performant', __FILE__
+rescue LoadError
+  require File.expand_path '../../ext/ruby19/extconf.rb', __FILE__
+  Dir.chdir File.expand_path('../../ext/ruby19', __FILE__) do
+    %x{ ruby extconf.rb && make }
+  end
+  retry
+end
