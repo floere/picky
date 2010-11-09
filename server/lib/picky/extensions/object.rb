173d67aeddfdf172de39d715aa8890c9fbfc93ab
Florian Hanke <florian.hanke@gmail.com>
2010-11-09 20:04:11 +0100

    + Kernel -> Object

--- server/lib/picky/extensions/object.rb
+++ server/lib/picky/extensions/object.rb
@@ -1,10 +1,14 @@
-module Kernel
+class Object
   
-  
+  # Puts a text in the form:
+  #   12:34:56: text here
+  #
   def timed_exclaim text
     exclaim "#{Time.now.strftime("%H:%M:%S")}: #{text}"
   end
   
+  # Just puts the given text.
+  #
   def exclaim text
     puts text
   end
