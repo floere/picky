2f8e5fae5baf3e94e1695b8e91faade7680dbf61
Florian Hanke <florian.hanke@gmail.com>
2010-11-25 21:18:21 +0100

    + specs for location search

--- server/test_project/spec/integration_spec.rb
+++ server/test_project/spec/integration_spec.rb
@@ -11,12 +11,18 @@ describe "Integration Tests" do
   before(:all) do
     Indexes.index_for_tests
     Indexes.load_from_cache
-    @full = Query::Full.new Indexes[:csv_test]
+    @csv = Query::Full.new Indexes[:csv_test]
+    @geo = Query::Full.new Indexes[:geo]
   end
   
-  def self.it_should_find_ids_in_main_full text, expected_ids
+  def self.it_should_find_ids_in_csv text, expected_ids
     it 'should return the right ids' do
-      @full.search_with_text(text).ids.should == expected_ids
+      @csv.search_with_text(text).ids.should == expected_ids
+    end
+  end
+  def self.it_should_find_ids_in_geo text, expected_ids
+    it 'should return the right ids' do
+      @geo.search_with_text(text).ids.should == expected_ids
     end
   end
   def ids_of results
@@ -26,74 +32,78 @@ describe "Integration Tests" do
   describe 'test cases' do
     # Standard
     #
-    it_should_find_ids_in_main_full 'Soledad Human', [72]
-    it_should_find_ids_in_main_full 'First Three Minutes Weinberg', [1]
+    it_should_find_ids_in_csv 'Soledad Human', [72]
+    it_should_find_ids_in_csv 'First Three Minutes Weinberg', [1]
     
     # Complex cases
     #
-    it_should_find_ids_in_main_full 'title:le* title:hystoree~', [4, 250, 428]
-    it_should_find_ids_in_main_full 'Hystori~ author:ferg', []
-    it_should_find_ids_in_main_full 'Hystori~ author:fergu', [4, 4]
-    it_should_find_ids_in_main_full 'Hystori~ author:fergus', [4, 4]
-    it_should_find_ids_in_main_full 'author:fergus', [4]
+    it_should_find_ids_in_csv 'title:le* title:hystoree~', [4, 250, 428]
+    it_should_find_ids_in_csv 'Hystori~ author:ferg', []
+    it_should_find_ids_in_csv 'Hystori~ author:fergu', [4, 4]
+    it_should_find_ids_in_csv 'Hystori~ author:fergus', [4, 4]
+    it_should_find_ids_in_csv 'author:fergus', [4]
     
     # Partial
     #
-    it_should_find_ids_in_main_full 'Gover* Systems', [7]
-    it_should_find_ids_in_main_full 'A*', [2, 3, 4, 5, 6, 7, 8, 15, 24, 27, 29, 35, 39, 52, 55, 63, 67, 76, 80, 101]
-    it_should_find_ids_in_main_full 'a* b* c* d* f', [110, 416]
+    it_should_find_ids_in_csv 'Gover* Systems', [7]
+    it_should_find_ids_in_csv 'A*', [2, 3, 4, 5, 6, 7, 8, 15, 24, 27, 29, 35, 39, 52, 55, 63, 67, 76, 80, 101]
+    it_should_find_ids_in_csv 'a* b* c* d* f', [110, 416]
     
     # Similarity
     #
-    it_should_find_ids_in_main_full 'Hystori~ Leeward', [4, 4]
-    it_should_find_ids_in_main_full 'Strutigic~ Guvurnance~', [7]
+    it_should_find_ids_in_csv 'Hystori~ Leeward', [4, 4]
+    it_should_find_ids_in_csv 'Strutigic~ Guvurnance~', [7]
     
     # Qualifiers
     #
-    it_should_find_ids_in_main_full "title:history author:fergus", [4]
+    it_should_find_ids_in_csv "title:history author:fergus", [4]
     
     # Splitting
     #
-    it_should_find_ids_in_main_full "history/fergus-history/fergus,history&fergus", [4, 4, 4, 4, 4, 4, 4, 4]
+    it_should_find_ids_in_csv "history/fergus-history/fergus,history&fergus", [4, 4, 4, 4, 4, 4, 4, 4]
     
     # Character Removal
     #
-    it_should_find_ids_in_main_full "'(history)' '(fergus)'", [4, 4]
+    it_should_find_ids_in_csv "'(history)' '(fergus)'", [4, 4]
     
     # Contraction
     #
-    # it_should_find_ids_in_main_full ""
+    # it_should_find_ids_in_csv ""
     
     # Stopwords
     #
-    it_should_find_ids_in_main_full "and the history or fergus", [4, 4]
-    it_should_find_ids_in_main_full "und and the or on of in is to from as at an history fergus", [4, 4]
+    it_should_find_ids_in_csv "and the history or fergus", [4, 4]
+    it_should_find_ids_in_csv "und and the or on of in is to from as at an history fergus", [4, 4]
     
     # Normalization
     #
-    # it_should_find_ids_in_main_full "Deoxyribonucleic Acid", []
-    # it_should_find_ids_in_main_full '800 dollars', []
+    # it_should_find_ids_in_csv "Deoxyribonucleic Acid", []
+    # it_should_find_ids_in_csv '800 dollars', []
     
     # Remove after splitting.
     #
-    # it_should_find_ids_in_main_full "history.fergus", [4, 4]
+    # it_should_find_ids_in_csv "history.fergus", [4, 4]
     
     # Character Substitution.
     #
-    it_should_find_ids_in_main_full "hïstôry Educåtioñ fërgus", [4, 4, 4, 4]
+    it_should_find_ids_in_csv "hïstôry Educåtioñ fërgus", [4, 4, 4, 4]
     
     # Breakage
     #
-    it_should_find_ids_in_main_full "%@{*^$!*$$^!&%!@%#!%#(#!@%#!#!)}", []
-    it_should_find_ids_in_main_full "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", []
+    it_should_find_ids_in_csv "%@{*^$!*$$^!&%!@%#!%#(#!@%#!#!)}", []
+    it_should_find_ids_in_csv "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", []
+    
+    # Location based search.
+    #
+    it_should_find_ids_in_geo "north2:47.5 east2:7.5", [3, 5]
     
     # Categorization.
     #
     it 'uses categorization correctly' do
-      @full.search_with_text('t:religion').ids.should == @full.search_with_text('title:religion').ids
+      @csv.search_with_text('t:religion').ids.should == @csv.search_with_text('title:religion').ids
     end
     it 'uses categorization' do
-      @full.search_with_text('title:religion').ids.should_not == @full.search_with_text('subject:religion').ids
+      @csv.search_with_text('title:religion').ids.should_not == @csv.search_with_text('subject:religion').ids
     end
   end
   
