ca375d68e6359a3f08c7c3339fa6d2c9365617d6
Florian Hanke <florian.hanke@gmail.com>
2010-12-09 22:20:19 +0100

    - trailing spaces (thanks tass)

--- server/project_prototype/app/application.rb
+++ server/project_prototype/app/application.rb
@@ -7,22 +7,22 @@
 # Ask me or the google group if you have questions or specific requests.
 #
 class PickySearch < Application
-  
+
   # Indexing: How text is indexed.
   #
   default_indexing removes_characters: /[^a-zA-Z0-9\s\/\-\"\&\.]/,
                    stopwords:          /\b(and|the|of|it|in|for)\b/,
                    splits_text_on:     /[\s\/\-\"\&\.]/
-                   
+
   # Querying: How query text is handled.
   #
   default_querying removes_characters: /[^a-zA-Z0-9\s\/\-\,\&\"\~\*\:]/, # Picky needs control chars *"~: to pass through.
                    stopwords:          /\b(and|the|of|it|in|for)\b/,
                    splits_text_on:     /[\s\/\-\,\&]+/,
-                   
+
                    maximum_tokens: 5, # Amount of tokens passing into a query (5 = default).
                    substitutes_characters_with: CharacterSubstituters::WestEuropean.new # Normalizes special user input, Ä -> Ae, ñ -> n etc.
-                   
+
   # Define an index. Use a database etc. source?
   # See http://github.com/floere/picky/wiki/Sources-Configuration#sources
   #
@@ -36,15 +36,15 @@ class PickySearch < Application
   books_index.define_category :year,
                               partial: Partial::None.new # Partial substring searching on the year does not make
                                                          # much sense, neither does similarity.
-  
+
   query_options = { :weights => { [:title, :author] => +3, [:title] => +1 } } # +/- points for ordered combinations.
-  
+
   full_books = Query::Full.new books_index, query_options    # A Full query returns ids, combinations, and counts.
   live_books = Query::Live.new books_index, query_options    # A Live query does return all that Full returns, except ids.
-  
+
   route %r{\A/books/full\Z} => full_books                    # Routing is simple: url_path_regexp => query
   route %r{\A/books/live\Z} => live_books                    # 
-  
+
   # Note: You can pass a query multiple indexes and it will query in all of them.
-  
+
 end
