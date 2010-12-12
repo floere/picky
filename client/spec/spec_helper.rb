b4e61d309301f4e65550ce03f4a36e211a08e80e
Florian Hanke <florian.hanke@gmail.com>
2010-12-12 14:08:30 +0100

    - unnecessary line in client spec helper

--- client/spec/spec_helper.rb
+++ client/spec/spec_helper.rb
@@ -1,6 +1,5 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV['SEARCH_ENV'] = 'test'
 require File.expand_path('../../lib/picky-client', __FILE__)
 require File.expand_path('../../lib/picky-client/generator', __FILE__)
 require 'spec'
