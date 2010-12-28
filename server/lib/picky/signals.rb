f3fa4165dec1da2d7f306fcd738cd6ae18fb875d
Florian Hanke <florian.hanke@gmail.com>
2010-12-28 20:06:54 +0100

    + Fixed and tested signals, however not using reload since memory handling is not yet ok

--- server/lib/picky/signals.rb
+++ server/lib/picky/signals.rb
@@ -1,11 +1,8 @@
 # TODO Cleanup and move to project_prototype.
 #
 # Signal.trap 'USR1' do
-#   Indexed.reload
+#   Indexes.reload
 # end
 # Signal.trap 'USR2' do
 #   Loader.reload
-# end
-# Signal.trap 'INT' do
-#   exit!
 # end
