3c54e7ed077ca81f4cea87194a4695a8426197db
Florian Hanke <florian.hanke@gmail.com>
2011-01-30 20:19:55 +0100

    + couch by default uses string/symbol keys

--- server/lib/picky/sources/couch.rb
+++ server/lib/picky/sources/couch.rb
@@ -9,21 +9,6 @@ module Sources
   
   # A Couch database source.
   #
-  # <b>IMPORTANT NOTE:
-  #
-  # Since Picky currently only handles integer ids (we're working on this),
-  # and CouchDB uses hexadecimal ids, this source automatically
-  # recalculates a couch id such as
-  # fa3f2577a8dbc6a91d7f9989cdffd38e
-  # into
-  # 332634873577882511228481564366832915342
-  # using String#hex.
-  #
-  # When using the integer ids in a webapp to get your
-  # objects from CouchDB, please do a Integer#to_s(16) on the
-  # ids you get from Picky before you use them to get your object from CouchDB.</b>
-  # 
-  #
   # Options:
   # * url
   # and all the options of a <tt>RestClient::Resource</tt>.
@@ -35,49 +20,6 @@ module Sources
   #
   class Couch < Base
     
-    # If your Couch DB uses UUID keys, use
-    #  Sources::Couch.new(:title, keys: Sources::Couch::UUIDKeys.new)
-    # Do not forget to reconvert the UUID Key from an integer in the client:
-    #  uuid = UUIDTools::UUID.parse_int(id)
-    #  uuid.to_s
-    #
-    class UUIDKeys
-      def initialize
-        # Tries to require the uuidtools gem.
-        #
-        begin
-          require 'uuidtools'
-        rescue LoadError
-          puts_gem_missing 'uuidtools', 'UUID keys in a CouchDB source'
-          exit 1
-        end
-      end
-      def to_i id
-        uuid = UUIDTools::UUID.parse id
-        uuid.to_i
-      end
-    end
-    
-    # If your Couch DB uses Hex keys, use
-    #  Sources::Couch.new(:title, keys: Sources::Couch::HexKeys.new)
-    # Do not forget to reconvert the Hex Key from an integer in the client:
-    #  id.to_s(16)
-    #
-    class HexKeys
-      def to_i id
-        id.hex
-      end
-    end
-    
-    # If your Couch DB uses Integer keys, use
-    #  Sources::Couch.new(:title, keys: Sources::Couch::IntegerKeys.new)
-    #
-    class IntegerKeys
-      def to_i id
-        id
-      end
-    end
-    
     #
     #
     def initialize *category_names, options
@@ -85,8 +27,6 @@ module Sources
       
       Hash === options && options[:url] || raise_no_db_given(category_names)
       @db = RestClient::Resource.new options.delete(:url), options
-      
-      @to_i_strategy = options.delete(:keys) || HexKeys.new
     end
     
     # Tries to require the rest_client gem.
@@ -106,7 +46,7 @@ module Sources
     def harvest type, category
       category_name = category.from.to_s
       get_data do |doc|
-        yield @to_i_strategy.to_i(doc[@@id_key]), doc[category_name] || next
+        yield doc[@@id_key].strip, doc[category_name] || next
       end
     end
     
