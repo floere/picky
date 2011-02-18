d110b73a6fe1d63972a8cb672aaf1a4afee99e99
Florian Hanke <florian.hanke@gmail.com>
2011-02-18 18:43:31 +0100

    + using client test to design new, simpler interface

---
+++ generators/client_test/app.rb
@@ -0,0 +1,67 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require
+
+# Load the "model".
+#
+require File.expand_path 'book', File.dirname(__FILE__)
+
+set :haml, { :format => :html5 }
+
+# Sets up two query instances.
+#
+FullBooks = Picky::Client::Full.new :host => 'localhost', :port => 8080, :path => '/books/full'
+LiveBooks = Picky::Client::Live.new :host => 'localhost', :port => 8080, :path => '/books/live'
+
+set :static, true
+set :public, File.dirname(__FILE__)
+set :views,  File.expand_path('views', File.dirname(__FILE__))
+
+# Root, the search interface.
+#
+get '/' do
+  @query = params[:q]
+  
+  haml :'/search'
+end
+
+# Configure. The configuration info page.
+#
+get '/configure' do
+  haml :'/configure'
+end
+
+# For full results, you get the ids from the picky server
+# and then populate the result with models (rendered, even).
+#
+get '/search/full' do
+  results = FullBooks.search params[:query], :offset => params[:offset]
+  results.extend Picky::Convenience
+  results.populate_with Book do |book|
+    book.to_s
+  end
+  
+  #
+  # Or use:
+  #   results.populate_with Book
+  #   
+  # Then:
+  #   rendered_entries = results.entries.map do |book| (render each book here) end
+  #
+  
+  ActiveSupport::JSON.encode results
+end
+
+# For live results, you'd actually go directly to the search server without taking the detour.
+#
+get '/search/live' do
+  LiveBooks.search params[:query], :offset => params[:offset]
+end
+
+helpers do
+  
+  def js path
+    "<script src='javascripts/#{path}.js' type='text/javascript'></script>"
+  end
+  
+end
---
+++ generators/client_test/book.rb
@@ -0,0 +1,42 @@
+require 'csv'
+
+# A book is simple, it has just:
+#  * a title
+#  * an author
+#  * an isbn
+#  * a publishing year
+#  * a publisher
+#  * a number of subjects
+#
+class Book
+  
+  @@books_mapping = {}
+  
+  # Load the books on startup.
+  #
+  file_name = File.expand_path 'library.csv', File.dirname(__FILE__)
+  CSV.open(file_name, 'r').each do |row|
+    @@books_mapping[row.shift.to_i] = row
+  end
+  
+  # Find uses a lookup table.
+  #
+  def self.find ids, _ = {}
+    ids.map { |id| new(id, *@@books_mapping[id]) }
+  end
+  
+  attr_reader :id
+  
+  def initialize id, title, author, year, publisher, subjects
+    @id, @title, @author, @year, @publisher, @subjects = id, title, author, year, publisher, subjects
+  end
+  
+  # "Rendering" ;)
+  #
+  # Note: This is just an example. Please do not render in the model.
+  #
+  def to_s
+    "<li class='book'><p>\"#{@title}\", by #{@author}</p><p>#{@year}, #{@publisher}</p><p>#{@subjects}</p></li>"
+  end
+  
+end
