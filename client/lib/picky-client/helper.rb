42336f921003b2b714e42e475c223c380cea1887
Florian Hanke <florian.hanke@gmail.com>
2011-02-18 17:37:04 +0100

    + 1.3.5 with split interface

--- client/lib/picky-client/helper.rb
+++ client/lib/picky-client/helper.rb
@@ -4,27 +4,37 @@ module Picky
   #
   class Helper
     
-    @@localized_interface = lambda { |options|
+    @@localized_input    = lambda { |options|
       search_button_text = options[:button]     || 'search'
-      no_results         = options[:no_results] || 'Sorry, no results found!'
-      more_allocations   = options[:more]       || 'more'
+<<-HTML
+<div class="dashboard empty">
+  <div class="feedback">
+    <div class="status"></div>
+    <input type="text" autocorrect="off" class="query"/>
+    <div class="reset" title="clear"></div>
+  </div>
+  <input type="button" class="search_button" value="#{search_button_text}">
+</div>
+HTML
+    }
+    @@localized_results = lambda { |options|
+      no_results        = options[:no_results] || 'Sorry, no results found!'
+      more_allocations  = options[:more]       || 'more'
+<<-HTML
+<ol class="results"></ol>
+<div class="no_results">#{no_results}</div>
+<div class="allocations">
+  <ol class="shown"></ol>
+  <ol class="more">#{more_allocations}</ol>
+  <ol class="hidden"></ol>
+</div>
+HTML
+    }
+    @@localized_interface = lambda { |options|
 <<-HTML
 <div id="picky">
-  <div class="dashboard empty">
-    <div class="feedback">
-      <div class="status"></div>
-      <input type="text" autocorrect="off" class="query"/>
-      <div class="reset" title="clear"></div>
-    </div>
-    <input type="button" class="search_button" value="#{search_button_text}">
-  </div>
-  <ol class="results"></ol>
-  <div class="no_results">#{no_results}</div>
-  <div class="allocations">
-    <ol class="shown"></ol>
-    <ol class="more">#{more_allocations}</ol>
-    <ol class="hidden"></ol>
-  </div>
+  #{@@localized_input[options]}
+  #{@@localized_results[options]}
 </div>
 HTML
     }
@@ -44,7 +54,13 @@ HTML
     #
     #
     def self.interface options = {}
-      @@localized_interface[options].gsub!(/[\n]/, '').squeeze! ' '
+      @@localized_interface[options]
+    end
+    def self.input options = {}
+      @@localized_input[options]
+    end
+    def self.results options = {}
+      @@localized_results[options]
     end
     
     # Returns a cached version if you always use a single language.
--- client/spec/picky-client/helper_spec.rb
+++ client/spec/picky-client/helper_spec.rb
@@ -17,12 +17,30 @@ describe Picky::Helper do
     end
   end
   
-  describe "interface" do
+  describe "input" do
     it "should return good html" do
-      Picky::Helper.interface.should == "<div id=\"picky\"> <div class=\"dashboard empty\"> <div class=\"feedback\"> <div class=\"status\"></div> <input type=\"text\" autocorrect=\"off\" class=\"query\"/> <div class=\"reset\" title=\"clear\"></div> </div> <input type=\"button\" class=\"search_button\" value=\"search\"> </div> <ol class=\"results\"></ol> <div class=\"no_results\">Sorry, no results found!</div> <div class=\"allocations\"> <ol class=\"shown\"></ol> <ol class=\"more\">more</ol> <ol class=\"hidden\"></ol> </div></div>"
+      Picky::Helper.input.should == "<div class=\"dashboard empty\">\n  <div class=\"feedback\">\n    <div class=\"status\"></div>\n    <input type=\"text\" autocorrect=\"off\" class=\"query\"/>\n    <div class=\"reset\" title=\"clear\"></div>\n  </div>\n  <input type=\"button\" class=\"search_button\" value=\"search\">\n</div>\n"
     end
     it "should return good html" do
-      Picky::Helper.interface(:button => 'find', :no_results => 'SORRY!', :more => 'Click for more!').should == "<div id=\"picky\"> <div class=\"dashboard empty\"> <div class=\"feedback\"> <div class=\"status\"></div> <input type=\"text\" autocorrect=\"off\" class=\"query\"/> <div class=\"reset\" title=\"clear\"></div> </div> <input type=\"button\" class=\"search_button\" value=\"find\"> </div> <ol class=\"results\"></ol> <div class=\"no_results\">SORRY!</div> <div class=\"allocations\"> <ol class=\"shown\"></ol> <ol class=\"more\">Click for more!</ol> <ol class=\"hidden\"></ol> </div></div>"
+      Picky::Helper.input(:button => 'find').should == "<div class=\"dashboard empty\">\n  <div class=\"feedback\">\n    <div class=\"status\"></div>\n    <input type=\"text\" autocorrect=\"off\" class=\"query\"/>\n    <div class=\"reset\" title=\"clear\"></div>\n  </div>\n  <input type=\"button\" class=\"search_button\" value=\"find\">\n</div>\n"
+    end
+  end
+  
+  describe "results" do
+    it "should return good html" do
+      Picky::Helper.input.should == "<div class=\"dashboard empty\">\n  <div class=\"feedback\">\n    <div class=\"status\"></div>\n    <input type=\"text\" autocorrect=\"off\" class=\"query\"/>\n    <div class=\"reset\" title=\"clear\"></div>\n  </div>\n  <input type=\"button\" class=\"search_button\" value=\"search\">\n</div>\n"
+    end
+    it "should return good html" do
+      Picky::Helper.input(:no_results => 'SORRY!', :more => 'Click for more!').should == "<div class=\"dashboard empty\">\n  <div class=\"feedback\">\n    <div class=\"status\"></div>\n    <input type=\"text\" autocorrect=\"off\" class=\"query\"/>\n    <div class=\"reset\" title=\"clear\"></div>\n  </div>\n  <input type=\"button\" class=\"search_button\" value=\"search\">\n</div>\n"
+    end
+  end
+  
+  describe "interface" do
+    it "should return good html" do
+      Picky::Helper.interface.should == "<div id=\"picky\">\n  <div class=\"dashboard empty\">\n  <div class=\"feedback\">\n    <div class=\"status\"></div>\n    <input type=\"text\" autocorrect=\"off\" class=\"query\"/>\n    <div class=\"reset\" title=\"clear\"></div>\n  </div>\n  <input type=\"button\" class=\"search_button\" value=\"search\">\n</div>\n\n  <ol class=\"results\"></ol>\n<div class=\"no_results\">Sorry, no results found!</div>\n<div class=\"allocations\">\n  <ol class=\"shown\"></ol>\n  <ol class=\"more\">more</ol>\n  <ol class=\"hidden\"></ol>\n</div>\n\n</div>\n"
+    end
+    it "should return good html" do
+      Picky::Helper.interface(:button => 'find', :no_results => 'SORRY!', :more => 'Click for more!').should == "<div id=\"picky\">\n  <div class=\"dashboard empty\">\n  <div class=\"feedback\">\n    <div class=\"status\"></div>\n    <input type=\"text\" autocorrect=\"off\" class=\"query\"/>\n    <div class=\"reset\" title=\"clear\"></div>\n  </div>\n  <input type=\"button\" class=\"search_button\" value=\"find\">\n</div>\n\n  <ol class=\"results\"></ol>\n<div class=\"no_results\">SORRY!</div>\n<div class=\"allocations\">\n  <ol class=\"shown\"></ol>\n  <ol class=\"more\">Click for more!</ol>\n  <ol class=\"hidden\"></ol>\n</div>\n\n</div>\n"
     end
   end
   
