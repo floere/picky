bab5593054879c7fa623b5b4e37abbe571e7bcfd
Florian Hanke <florian.hanke@gmail.com>
2011-02-21 00:25:19 +0100

    - note

--- server/lib/picky/index/redis.rb
+++ server/lib/picky/index/redis.rb
@@ -7,10 +7,6 @@ module Index
     def initialize bundle_name, config
       super bundle_name, config
       
-      # Note: We marshal the similarity, as the
-      #       Yajl json lib cannot load symbolized
-      #       values, just keys.
-      #
       # TODO
       #
       @index         = Redis::ListHash.new "#{config.identifier}:#{bundle_name}:index"
