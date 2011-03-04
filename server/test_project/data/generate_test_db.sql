9b2d49eb1085a8a38cddfe56e7fee22f1be96105
Florian Hanke <florian.hanke@gmail.com>
2011-03-04 19:00:56 +0100

    - generating the test DB

--- server/test_project/data/generate_test_db.sql
+++ server/test_project/data/generate_test_db.sql
@@ -1,14 +1,19 @@
+# MySQL data
+#
+# On Florian's local computer:
+# mysql --user developer -D picky_test_project < /Users/admin/temp/picky/server/test_project/data/generate_test_db.sql
+#
 DROP TABLE IF EXISTS books;
 
--- .schema test
 CREATE TABLE books (
   id     integer      not null,
   title  varchar(100) not null,
   author varchar(100) not null,
-  blurb  varchar(400) not null,
   isbn   varchar(10)  not null,
-  year   number(4)    not null
+  year   decimal(4)   not null
 );
 
-.mode csv
-.import data/books.csv books
+LOAD DATA INFILE '/Users/admin/temp/picky/server/test_project/data/books.csv'
+  INTO TABLE books
+  FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+    LINES TERMINATED BY '\n';
