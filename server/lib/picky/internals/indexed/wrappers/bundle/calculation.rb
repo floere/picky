088de19610b5a1c5069a0cd5def9a45f6e540e15
Florian Hanke <florian.hanke@gmail.com>
2011-04-05 03:49:46 +0200

    + Note

--- server/lib/picky/internals/indexed/wrappers/bundle/calculation.rb
+++ server/lib/picky/internals/indexed/wrappers/bundle/calculation.rb
@@ -1,35 +1,35 @@
 module Indexed
   module Wrappers
-    
+
     module Bundle
-      
+
       # A calculation rewrites the symbol into a float.
       #
-      # TODO I really need to allow integers as keys. The code below is just not ok.
+      # TODO I really need to allow integers as keys. The code below is just not up to the needed quality.
       #
       class Calculation < Wrapper
-        
+
         #
         #
         def recalculate float
           float
         end
-        
+
         #
         #
         def ids sym
           @bundle.ids recalculate(sym.to_s.to_f).to_s.to_sym
         end
-        
+
         #
         #
         def weight sym
           @bundle.weight recalculate(sym.to_s.to_f).to_s.to_sym
         end
-        
+
       end
-      
+
     end
-    
+
   end
 end
