697baf2fe9563a002d3e676f6dad53c4ad905f54
Florian Hanke <florian.hanke@gmail.com>
2011-04-06 15:57:04 +1000

    - TODO

--- server/lib/picky/internals/query/combinations/memory.rb
+++ server/lib/picky/internals/query/combinations/memory.rb
@@ -8,12 +8,12 @@ module Internals
     # An allocation consists of a number of combinations.
     #
     module Combinations # :nodoc:all
-    
+
       # Memory Combinations contain specific methods for
       # calculating score and ids in memory.
       #
       class Memory < Base
-      
+
         # Returns the result ids for the allocation.
         #
         # Sorts the ids by size and & through them in the following order (sizes):
@@ -24,7 +24,7 @@ module Internals
         # Note: Uses a C-optimized intersection routine for speed and memory efficiency.
         #
         # Note: In the memory based version we ignore the (amount) needed hint.
-        # TODO Not ignore it?
+        #       We might use the fact to optimize the algorithm.
         #
         def ids _, _
           return [] if @combinations.empty?
@@ -43,16 +43,16 @@ module Internals
           #      this precondition for a fast algorithm is always given.
           #
           id_arrays.sort! { |this_array, that_array| this_array.size <=> that_array.size }
-      
+
           # Call the optimized C algorithm.
           #
           Performant::Array.memory_efficient_intersect id_arrays
         end
-      
+
       end
-    
+
     end
-  
+
   end
-  
+
 end
