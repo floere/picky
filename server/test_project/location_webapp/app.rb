8a18cacd97b6c748d1e803ad8d3134044dacde41
Florian Hanke <florian.hanke@gmail.com>
2011-04-06 15:57:19 +1000

    ! test project webapp

--- server/test_project/location_webapp/app.rb
+++ server/test_project/location_webapp/app.rb
@@ -10,8 +10,7 @@ set :haml, { :format => :html5 }
 
 # Sets up two query instances.
 #
-FullGeo = Picky::Client::Full.new :host => 'localhost', :port => 8080, :path => '/geo/full'
-LiveGeo = Picky::Client::Live.new :host => 'localhost', :port => 8080, :path => '/geo/live'
+Geo = Picky::Client.new :host => 'localhost', :port => 8080, :path => '/geo'
 
 set :static, true
 set :public, File.dirname(__FILE__)
@@ -33,33 +32,33 @@ end
 # and then populate the result with models (rendered, even).
 #
 get '/search/full' do
-  results = FullGeo.search :query => params[:query], :offset => params[:offset]
+  results = Geo.search params[:query], :ids => 20, :offset => params[:offset]
   results.extend Picky::Convenience
   results.populate_with Location do |location|
     location.to_s
   end
-  
+
   #
   # Or use:
   #   results.populate_with Book
-  #   
+  #
   # Then:
   #   rendered_entries = results.entries.map do |book| (render each book here) end
   #
-  
+
   ActiveSupport::JSON.encode results
 end
 
 # For live results, you'd actually go directly to the search server without taking the detour.
 #
 get '/search/live' do
-  LiveGeo.search :query => params[:query], :offset => params[:offset]
+  Geo.search_unparsed params[:query], :ids => 0, :offset => params[:offset]
 end
 
 helpers do
-  
+
   def js path
     "<script src='javascripts/#{path}.js' type='text/javascript'></script>"
   end
-  
+
 end
