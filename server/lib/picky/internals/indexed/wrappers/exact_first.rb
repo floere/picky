f5c7e982107c69e433381efeb1032b42e02cf14c
Florian Hanke <florian.hanke@gmail.com>
2011-04-06 15:57:31 +1000

    + wrapper should be subclass of indexed base class, not a specific index type

--- server/lib/picky/internals/indexed/wrappers/exact_first.rb
+++ server/lib/picky/internals/indexed/wrappers/exact_first.rb
@@ -3,18 +3,18 @@ module Internals
   # encoding: utf-8
   #
   module Indexed
-  
+
     # TODO Spec
     #
     module Wrappers
-    
+
       # This index combines an exact and partial index.
       # It serves to order the results such that exact hits are found first.
       #
       # TODO Need to use the right subtokens. Bake in?
       #
-      class ExactFirst < Indexed::Bundle::Memory
-      
+      class ExactFirst < Indexed::Bundle::Base
+
         delegate :similar,
                  :identifier,
                  :name,
@@ -28,12 +28,12 @@ module Internals
                  :dump,
                  :load,
                  :to => :@partial
-      
+
         def initialize category
           @exact   = category.exact
           @partial = category.partial
         end
-      
+
         def self.wrap index_or_category
           if index_or_category.respond_to? :categories
             wrap_each_of index_or_category.categories
@@ -47,19 +47,19 @@ module Internals
         def self.wrap_each_of categories
           categories.categories.collect! { |category| new(category) }
         end
-      
+
         def ids text
           @exact.ids(text) + @partial.ids(text)
         end
-      
+
         def weight text
           [@exact.weight(text) || 0, @partial.weight(text) || 0].max
         end
-      
+
       end
-    
+
     end
-  
+
   end
-  
+
 end
