2678f406e25eab5d9c2031ab58748a97b0c3f561
Florian Hanke <florian.hanke@gmail.com>
2011-04-08 19:14:33 +1000

    + aliases specs

--- server/spec/lib/generators/aliases_spec.rb
+++ server/spec/lib/generators/aliases_spec.rb
@@ -1,1 +1,15 @@
-# TODO aliases spec.
+require 'spec_helper'
+
+describe 'aliases' do
+
+  it 'aliases correctly' do
+    Partial.should == Internals::Generators::Partial
+  end
+  it 'aliases correctly' do
+    Similarity.should == Internals::Generators::Similarity
+  end
+  it 'aliases correctly' do
+    Weights.should == Internals::Generators::Weights
+  end
+  
+end
