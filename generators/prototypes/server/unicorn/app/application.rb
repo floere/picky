b2b43b2d36235c396ab04fbe29485b22df80a27a
Florian Hanke <florian.hanke@gmail.com>
2011-04-14 11:53:25 +1000

    + generated examples now use the new style APU

--- generators/prototypes/server/unicorn/app/application.rb
+++ generators/prototypes/server/unicorn/app/application.rb
@@ -9,20 +9,20 @@ class PickySearch < Application
 
   # How text is indexed.
   #
-  default_indexing removes_characters: /[^a-zA-Z0-9\s\/\-\"\&\.]/i,
-                   stopwords:          /\b(and|the|of|it|in|for)\b/i,
-                   splits_text_on:     /[\s\/\-\"\&\.]/
+  indexing removes_characters: /[^a-zA-Z0-9\s\/\-\"\&\.]/i,
+           stopwords:          /\b(and|the|of|it|in|for)\b/i,
+           splits_text_on:     /[\s\/\-\"\&\.]/
 
   # How query text is preprocessed.
   #
-  default_querying removes_characters: /[^a-zA-Z0-9\s\/\-\,\&\"\~\*\:]/i, # Picky needs control chars *"~: to pass through.
-                   stopwords:          /\b(and|the|of|it|in|for)\b/i,
-                   splits_text_on:     /[\s\/\-\,\&]+/,
-                   maximum_tokens: 5, # Amount of tokens used in a search (5 = default).
-                   substitutes_characters_with: CharacterSubstituters::WestEuropean.new # Normalizes special user input, Ä -> Ae, ñ -> n etc.
+  searching removes_characters: /[^a-zA-Z0-9\s\/\-\,\&\"\~\*\:]/i, # Picky needs control chars *"~: to pass through.
+            stopwords:          /\b(and|the|of|it|in|for)\b/i,
+            splits_text_on:     /[\s\/\-\,\&]+/,
+            maximum_tokens: 5, # Amount of tokens used in a search (5 = default).
+            substitutes_characters_with: CharacterSubstituters::WestEuropean.new # Normalizes special user input, Ä -> Ae, ñ -> n etc.
 
-  library_src = Sources::CSV.new :title, :author, :year, file: 'app/library.csv'
-  books_index = Index::Memory.new :books, library_src do
+  books_index = Index::Memory.new :books do
+    source   Sources::CSV.new(:title, :author, :year, file: 'app/library.csv')
     category :title,
              similarity: Similarity::DoubleMetaphone.new(3), # Default is no similarity.
              partial: Partial::Substring.new(from: 1) # Default is from: -3.
@@ -30,6 +30,8 @@ class PickySearch < Application
     category :year, partial: Partial::None.new
   end
 
-  route %r{\A/books\Z} => Search.new(books_index, :weights => { [:title, :author] => +3, [:title] => +1 })
+  route %r{\A/books\Z} => Search.new(books_index) do
+    boost [:title, :author] => +3, [:title] => +1
+  end
 
 end
