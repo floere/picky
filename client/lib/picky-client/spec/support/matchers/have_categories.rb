1edc4eeb37e276e2b3f3946f932db02e66a4e374
Florian Hanke <florian.hanke@gmail.com>
2011-04-17 20:05:09 +1000

    + Cater to both the old Spec and the new RSpec

--- client/lib/picky-client/spec/support/matchers/have_categories.rb
+++ client/lib/picky-client/spec/support/matchers/have_categories.rb
@@ -1,29 +1,25 @@
-# TODO
-#
-if defined?(Spec)
-  Spec::Matchers.define :have_categories do |*expected|
+(defined?(Spec) && Spec || defined?(RSpec) && RSpec)::Matchers.define :have_categories do |*expected|
 
-    match do |results|
-      extract_categories(actual) == expected
-    end
-
-    failure_message_for_should do |results|
-      "expected that #{extract_categories(results)} would be named and ordered as #{expected}"
-    end
-
-    failure_message_for_should_not do |results|
-      "expected that #{extract_categories(results)} would not be named and ordered as #{expected}"
-    end
-
-    description do
-      "be categories named and ordered as #{expected}"
-    end
-
-    def extract_categories results
-      results.allocations.map do |allocation|
-        allocation[3].map { |combination| combination[0] }
-      end
-    end
-
+  match do |results|
+    extract_categories(actual) == expected
   end
+
+  failure_message_for_should do |results|
+    "expected categories #{extract_categories(results)} to be named and ordered as #{expected}"
+  end
+
+  failure_message_for_should_not do |results|
+    "expected categories #{extract_categories(results)} not to be named and ordered as #{expected}"
+  end
+
+  description do
+    "be categories named and ordered as #{expected}"
+  end
+
+  def extract_categories results
+    results.allocations.map do |allocation|
+      allocation[3].map { |combination| combination[0] }
+    end
+  end
+
 end
