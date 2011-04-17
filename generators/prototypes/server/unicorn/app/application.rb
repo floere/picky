3365b28539f24b74dd78c4b7d864e3a71146a4e3
Florian Hanke <florian.hanke@gmail.com>
2011-04-17 21:30:15 +1000

    + use the right data file

--- generators/prototypes/server/unicorn/app/application.rb
+++ generators/prototypes/server/unicorn/app/application.rb
@@ -22,7 +22,7 @@ class PickySearch < Application
             substitutes_characters_with: CharacterSubstituters::WestEuropean.new # Normalizes special user input, Ä -> Ae, ñ -> n etc.
 
   books_index = Index::Memory.new :books do
-    source   Sources::CSV.new(:title, :author, :year, file: 'app/library.csv')
+    source   Sources::CSV.new(:title, :author, :year, file: "data/#{PICKY_ENVIRONMENT}/library.csv")
     category :title,
              similarity: Similarity::DoubleMetaphone.new(3), # Default is no similarity.
              partial: Partial::Substring.new(from: 1) # Default is from: -3.
