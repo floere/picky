65d8aa54ce44a4385a0d99f1a08fbed21ae92294
Florian Hanke <florian.hanke@gmail.com>
2011-04-17 19:38:52 +1000

    + test client, matchers

---
+++ client/lib/picky-client/spec.rb
@@ -0,0 +1,3 @@
+require File.expand_path '../../picky-client', __FILE__
+require File.expand_path '../spec/test_client', __FILE__
+require File.expand_path '../spec/support/matchers/have_categories', __FILE__
---
+++ client/lib/picky-client/spec/test_client.rb
@@ -0,0 +1,36 @@
+module Picky
+
+  class TestClient < Client
+
+    attr_reader :request
+
+    #
+    #
+    # Example:
+    #   Picky::TestClient.new(BookSearch, :path => '/books')
+    #
+    def initialize rack_app, options = {}
+      super options
+
+      @request = Rack::MockRequest.new rack_app
+    end
+
+    # Wraps the search method to always extend the result with Convenience.
+    #
+    def search query, params = {}
+      result = super
+      result.extend Convenience
+      result
+    end
+
+    # Backend method that we override to not send a real search.
+    #
+    def send_search params = {}
+      params = defaultize params
+
+      request.get("#{self.path}?#{params.to_query}").body
+    end
+
+  end
+
+end
