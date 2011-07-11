fc0f67f21b7039090460182938bedacff81151cd
Florian Hanke <florian.hanke@gmail.com>
2011-07-11 23:05:57 +1000

    + qualifier category mapper

---
+++ server/lib/picky/query/qualifier_category_mapper.rb
@@ -0,0 +1,39 @@
+# coding: utf-8
+#
+module Query
+
+  # Collection class for qualifiers.
+  #
+  class QualifierCategoryMapper # :nodoc:all
+
+    attr_reader :mapping
+
+    #
+    #
+    def initialize
+      @mapping = {}
+    end
+
+    #
+    #
+    def add category
+      category.qualifiers.each do |qualifier|
+        sym_qualifier = qualifier.to_sym
+        warn %Q{Warning: Qualifier "#{qualifier}" already mapped to category #{mapping[sym_qualifier].identifier} (ambiguous qualifier mapping).} if mapping.has_key? sym_qualifier
+        mapping[sym_qualifier] = category
+      end
+    end
+
+    # Normalizes the given qualifier.
+    #
+    # Returns nil if it is not allowed, the normalized qualifier if it is.
+    #
+    def map qualifier
+      return nil if qualifier.blank?
+
+      @mapping[qualifier.to_sym]
+    end
+
+  end
+
+end
---
+++ server/spec/lib/query/qualifier_category_mapper_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe Query::QualifierCategoryMapper do
+  
+  let(:mapper) { described_class.new }
+  before(:each) do
+    @category1 = stub(:category1, :qualifiers => ['t1', 'tt1', 'ttt1'])
+    @category2 = stub(:category2, :qualifiers => [:t2, :tt2, :ttt2])
+    @category3 = stub(:category3, :qualifiers => [:t3, :tt3, :ttt3])
+    mapper.add @category1
+    mapper.add @category2
+    mapper.add @category3
+  end
+  
+  def self.it_should_map(qualifier, expected)
+    it "should map #{qualifier} to #{expected}" do
+      mapper.map(qualifier).should == expected
+    end
+  end
+
+  describe "mapping" do
+    it { mapper.map(:t1).should   == @category1 }
+    it { mapper.map(:tt1).should  == @category1 }
+    it { mapper.map(:ttt1).should == @category1 }
+    
+    it { mapper.map(:t2).should   == @category2 }
+    it { mapper.map(:tt2).should  == @category2 }
+    it { mapper.map(:ttt2).should == @category2 }
+    
+    it { mapper.map(:t3).should   == @category3 }
+    it { mapper.map(:tt3).should  == @category3 }
+    it { mapper.map(:ttt3).should == @category3 }
+  end
+end
