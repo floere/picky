332193e143c9cf74fb46918a451179bad0a80b4d
Florian Hanke <florian.hanke@gmail.com>
2011-07-30 01:53:18 +1000

    + load the application before testing

--- server/test_project_sinatra/spec/spec_helper.rb
+++ server/test_project_sinatra/spec/spec_helper.rb
@@ -2,7 +2,9 @@ ENV['PICKY_ENV'] = 'test'
 
 require File.expand_path '../../../lib/picky', __FILE__
 
+# TODO
+#
 # SearchLog = Picky::Loggers::Search.new ::Logger.new(STDOUT)
 # puts "Using STDOUT as test log."
-#
-# Picky::Loader.load_application
+
+Picky::Loader.load_application
