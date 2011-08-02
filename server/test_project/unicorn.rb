ec5f9266eed1f29aa270882a9ab50b3bf58ec675
Florian Hanke <florian.hanke@gmail.com>
2011-08-02 23:22:30 +1000

    + unicorn.rb instead of unicorn.ru

---
+++ server/test_project/unicorn.rb
@@ -0,0 +1,9 @@
+listen            '0.0.0.0:8080'
+pid               'tmp/pids/unicorn.pid'
+preload_app       true
+timeout           10
+worker_processes  3
+
+# after_fork do |_, _|
+#   GC.disable
+# end
