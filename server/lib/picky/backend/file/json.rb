84f6dabe808d26383b06c9b8d3938f0e282fe69b
Florian Hanke <florian.hanke@gmail.com>
2011-08-08 19:45:54 +1000

    + a possible circumvention of the yajl symbolize_keys problem

--- server/lib/picky/backend/file/json.rb
+++ server/lib/picky/backend/file/json.rb
@@ -17,6 +17,10 @@ module Picky
         #
         def load
           Yajl::Parser.parse ::File.open(cache_path, 'r'), symbolize_keys: true
+          # Yajl::Parser.parse(::File.open(cache_path, 'r')).inject({}) do |hash, (k, v)| #, symbolize_keys: true
+          #   hash[k.to_sym] = v
+          #   hash
+          # end
         end
         # Dumps the index hash in json format.
         #
