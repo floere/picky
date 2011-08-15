af0c2d7c35dfc97b9a734298c0bc4d821c8d6fe9
Florian Hanke <florian.hanke@gmail.com>
2011-08-15 14:41:59 +1000

    + Notes on location calculations

--- server/lib/picky/indexed/wrappers/bundle/calculation.rb
+++ server/lib/picky/indexed/wrappers/bundle/calculation.rb
@@ -7,11 +7,19 @@ module Picky
 
         # A calculation rewrites the symbol into a float.
         #
-        # TODO I really need to allow integers as keys. The code below is just not up to the needed quality.
+        # Note: A calculation will try to find a float in the index,
+        #       not a sym.
+        #
+        # TODO I really need to allow integers as keys.
+        #      The code below is just not up to the needed quality.
+        #      Use key_format :to_i?
         #
         class Calculation < Wrapper
 
+          # API.
           #
+          # By default, a calculation does not
+          # recalculate anything.
           #
           def recalculate float
             float
@@ -19,14 +27,14 @@ module Picky
 
           #
           #
-          def ids sym
-            @bundle.ids recalculate(sym.to_s.to_f).to_s.to_sym
+          def ids float_as_sym
+            @bundle.ids recalculate(float_as_sym.to_s.to_f).to_s.to_sym
           end
 
           #
           #
-          def weight sym
-            @bundle.weight recalculate(sym.to_s.to_f).to_s.to_sym
+          def weight float_as_sym
+            @bundle.weight recalculate(float_as_sym.to_s.to_f).to_s.to_sym
           end
 
         end
