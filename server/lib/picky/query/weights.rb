d328449356c329ac30f19b9df9ac76864ea5ad22
Florian Hanke <florian.hanke@gmail.com>
2011-08-15 12:24:52 +1000

    + straighten internal API for weights/boosting

--- server/lib/picky/query/weights.rb
+++ server/lib/picky/query/weights.rb
@@ -2,51 +2,77 @@ module Picky
 
   module Query
 
-    # Calculates weights for certain combinations.
+    # Calculates scores/weights for combinations.
     #
-    class Weights # :nodoc:all
+    # Example:
+    #   Someone searches for peter fish.
+    #   Picky might match this to categories as follows:
+    #     [:name, :food]
+    #   and
+    #     [:name, :surname]
+    #
+    # This class is concerned with calculating scores
+    # for the category combinations.
+    #
+    # Implement either
+    #   #score_for(combinations)
+    # or
+    #   #weight_for(category_names) # Subclass this class for this.
+    #
+    # And return a weight.
+    #
+    class Weights
 
       attr_reader :weights
 
-      delegate :empty?, :to => :weights
+      delegate :empty?,
+               :to => :weights
 
-      #
+      # Needs a Hash of
+      #   [:category_name1, :category_name2] => +3
+      # (some positive or negative weight)
       #
       def initialize weights = {}
         @weights = weights
       end
 
-      # Get the weight of an allocation.
+      # API.
       #
-      def weight_for clustered
-        @weights[clustered] || 0
+      # Get the weight for an array of category names.
+      #
+      # Example:
+      #   [:name, :height, :color] returns +3, but
+      #   [:name, :height, :street] returns -1.
+      #
+      # Note: Use Array#clustered_uniq_fast to make
+      #       [:a, :a, :b, :a] => [:a, :b, :a]
+      #
+      def weight_for category_names
+        @weights[category_names.clustered_uniq_fast] || 0
       end
 
-      # Returns an energy term E for allocation. this turns into a probability
-      # by P(allocation) = 1/Z * exp (-1/T * E(allocation)),
-      # where Z is the normalizing partition function
-      # sum_allocations exp(-1/T *E(allocation)), and T is a temperature constant.
-      # If T is high the distribution will be close to equally distributed.
-      # If T is low, the distribution will be the indicator function
-      # for min (E(allocation))…
+      # API.
       #
-      # ...
-      #
-      # Just kidding. It's far more complicated than that. Ha ha ha ha ;)
+      # Calculates a score for the combinations.
+      # Implement #weight_for(category_names) if you don't need the
+      # actual combinations, just the category names.
       #
       # Note: Cache this if more complicated weighings become necessary.
+      # Note: Maybe make combinations comparable to Symbols?
       #
-      def score combinations
-        # TODO Or it could use actual combinations? Could it? Or make combinations comparable to Symbols.
-        #
-        weight_for combinations.map(&:category_name).clustered_uniq_fast
+      def score_for combinations
+        weight_for combinations.map(&:category_name)
       end
 
+      # A Weights instance is == to another if
+      # the weights are the same.
+      #
       def == other
         @weights == other.weights
       end
 
-      # Prints out a nice representation of the configured weights.
+      # Prints out a nice representation of the
+      # configured weights.
       #
       def to_s
         "#{self.class}(#{@weights})"
--- server/spec/lib/query/weights_spec.rb
+++ server/spec/lib/query/weights_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Picky::Query::Weights do
-
+  
   context 'with weights' do
     before(:each) do
       @weights = described_class.new [:test1, :test2]         => 6,
@@ -18,7 +18,18 @@ describe Picky::Query::Weights do
                                      [:test3, :test1]         => 2,
                                      [:test1, :test3]         => 2
     end
-
+    
+    describe 'score_for' do
+      it 'gets the category names from the combinations' do
+        combinations = [
+          stub(:combination1, :category_name => :test1),
+          stub(:combination1, :category_name => :test2)
+        ]
+        
+        @weights.score_for(combinations).should == +6
+      end
+    end
+    
     describe "weight_for" do
       it "should return zero if there is no specific weight" do
         @weights.weight_for([:not_a_specific_allocation]).should be_zero
