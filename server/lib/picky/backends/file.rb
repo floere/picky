27bf3db693da89b1bcb210acd1598c21a56ba7be
Florian Hanke <florian.hanke@gmail.com>
2011-08-28 09:36:40 +1000

    + doc

--- server/lib/picky/backends/file.rb
+++ server/lib/picky/backends/file.rb
@@ -5,20 +5,32 @@ module Picky
     # Naive implementation of a file-based index.
     # In-Memory Hash with length, offset:
     #   { :bla => [20, 312] }
-    # That map to positions the File, encoded in JSON?:
+    # That map to positions the File, encoded in JSON:
     #   ...[1,2,3,21,7,4,13,15]...
     #
     class File < Backend
 
+      # Returns an object that responds to:
+      #   [:token] => [id, id, id, id, id] # (an array of ids)
+      #
       def create_inverted bundle
         JSON.new bundle.index_path(:inverted)
       end
+      # Returns an object that responds to:
+      #   [:token] => 1.23 # (a weight)
+      #
       def create_weights bundle
         JSON.new bundle.index_path(:weights)
       end
+      # Returns an object that responds to:
+      #   [:encoded] => [:original, :original] # (an array of original symbols this similarity encoded thing maps to)
+      #
       def create_similarity bundle
         JSON.new bundle.index_path(:similarity)
       end
+      # Returns an object that responds to:
+      #   [:key] => value # (a value for this config key)
+      #
       def create_configuration bundle
         JSON.new bundle.index_path(:configuration)
       end
