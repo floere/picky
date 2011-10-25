db37dce4887828f1367a39d5c8cc3e19445dd9f7
Florian Hanke <florian.hanke@gmail.com>
2011-10-26 09:56:39 +1100

    + pushed the #add method to where it belongs

--- server/lib/picky/categories_realtime.rb
+++ server/lib/picky/categories_realtime.rb
@@ -3,18 +3,9 @@ module Picky
   class Categories
 
     each_delegate :remove,
+                  :add,
                   :replace,
                   :to => :categories
-
-    # TODO Rewrite indexing/tokenizer/caching etc.
-    #
-    def add object
-      id = object.id
-      categories.each do |category|
-        tokens, _ = category.tokenizer.tokenize object.send(category.from).to_s
-        category.add_tokenized id, tokens
-      end
-    end
 
   end
 
