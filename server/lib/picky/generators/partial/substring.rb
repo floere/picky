982ffe8b394466d878210e8b2507311c3ae783a2
Florian Hanke <florian.hanke@gmail.com>
2011-11-11 00:07:40 +1100

    - TODOs

--- server/lib/picky/generators/partial/substring.rb
+++ server/lib/picky/generators/partial/substring.rb
@@ -21,12 +21,10 @@ module Picky
             if @from < 0 && @to < 0
               def each_subtoken token, &block
                 token.each_subtoken @from - @to - 1, (0..@to), &block
-                # token[0..@to].intern.each_subtoken @from - @to - 1, &block # TODO to_sym
               end
             else
               def each_subtoken token, &block
                 token.each_subtoken @from, (0..@to), &block
-                # token[0..@to].intern.each_subtoken @from, &block # TODO to_sym
               end
             end
           end
