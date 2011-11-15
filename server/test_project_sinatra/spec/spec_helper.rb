a2625e577e5ca3d8dd0d2c3f5083a4562f621db3
Florian Hanke <florian.hanke@gmail.com>
2011-11-15 22:41:09 +1100

    + nicer output on what to do

--- server/test_project_sinatra/spec/spec_helper.rb
+++ server/test_project_sinatra/spec/spec_helper.rb
@@ -4,7 +4,7 @@ require File.expand_path '../../../lib/picky', __FILE__
 
 # Check Redis.
 #
-system('redis-cli quit') || fail('Redis must be running.')
+system('redis-cli quit') || fail('Redis must be running. Run redis-server in a terminal.')
 
 # Prepare test DB.
 #
@@ -12,6 +12,7 @@ system('redis-cli quit') || fail('Redis must be running.')
 #       Please change and rerun. And suggest a better solution, please :)
 #
 sql_file = File.expand_path '../../data/generate_test_db.sql', __FILE__
-system("mysql --user developer -D picky_test_project < #{sql_file}") || fail("MySQL test data couldn't be inserted.")
+system("mysql --user developer -D picky_test_project < #{sql_file}") ||
+  fail("MySQL test data couldn't be inserted. Start a MySQL server using mysql.server start")
 
 Picky::Loader.load_application
