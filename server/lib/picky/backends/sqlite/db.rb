a4258be5ebae85df152fd280f5050bd5becb8135
Florian Hanke <florian.hanke@gmail.com>
2011-11-16 13:11:08 +1100

    + some tests, + slightly rewrote the SQLite client

--- server/lib/picky/backends/sqlite/db.rb
+++ server/lib/picky/backends/sqlite/db.rb
@@ -8,7 +8,7 @@ module Picky
 
         include Helpers::File
 
-        attr_reader :cache_path
+        attr_reader :cache_path, :db
 
         def initialize cache_path, options = {}
           @cache_path = "#{cache_path}.sqlite3"
@@ -24,40 +24,32 @@ module Picky
           @empty && @empty.clone || {}
         end
 
-        def dump_sqlite internal
+        def lazily_initialize_client
           @db ||= SQLite3::Database.new cache_path
+        end
+
+        def dump_sqlite internal
+          lazily_initialize_client
 
           # TODO Does it make a difference if these
           #      statements are given as one to the
           #      @db.execute method?
           #
 
-          @db.execute <<-SQL
-            drop table if exists key_value;
-          SQL
-
-          @db.execute <<-SQL
-            create table key_value (
-              key varchar(255),
-              value text
-            );
-          SQL
-
-          @db.execute <<-SQL
-            create index key_idx on key_value (key);
-          SQL
-
-          @db.execute "BEGIN;"
+          db.execute 'drop table if exists key_value;'
+          db.execute 'create table key_value (key varchar(255), value text);'
+          db.execute 'create index key_idx on key_value (key);'
+          db.execute 'BEGIN;'
 
           # Note: Internal structures need to
           #       implement each.
           #
           internal.each do |key, value|
             encoded_value = Yajl::Encoder.encode value
-            @db.execute "insert into key_value values (?,?)", key.to_s, encoded_value
+            db.execute 'insert into key_value values (?,?)', key.to_s, encoded_value
           end
 
-          @db.execute "COMMIT;"
+          db.execute 'COMMIT;'
         end
 
         def dump internal
@@ -66,12 +58,12 @@ module Picky
         end
 
         def load
-          @db ||= SQLite3::Database.new cache_path
+          lazily_initialize_client
           self
         end
 
         def [] key
-          res = @db.execute "select value from key_value where key = ? limit 1;", key.to_s
+          res = db.execute "select value from key_value where key = ? limit 1;", key.to_s
           return nil if res.empty?
 
           # TODO Slightly speed up by not calling Yajl for the || case?
