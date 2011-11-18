6681a9b8376318185a2a57515eb8ceaa013fa5d7
Florian Hanke <florian.hanke@gmail.com>
2011-11-19 02:18:33 +1100

    + integration specs

--- generators/prototypes/server/shared/spec/integration_spec.rb
+++ generators/prototypes/server/shared/spec/integration_spec.rb
@@ -4,25 +4,25 @@ require 'spec_helper'
 require 'picky-client/spec'
 
 describe 'Integration Tests' do
-  
+
   before(:all) do
     Picky::Indexes.index_for_tests
-    Picky::Indexes.load_from_cache
+    Picky::Indexes.load
   end
-  
+
   let(:books) { Picky::TestClient.new(BookSearch, :path => '/books') }
 
   # Testing a count of results.
   #
   it { books.search('a s').total.should == 42 }
-  
+
   # Testing a specific order of result ids.
   #
   it { books.search('alan').ids.should == [449, 259, 307] }
-  
+
   # Testing an order of result categories.
   #
   it { books.search('alan').should have_categories(['title'], ['author']) }
   it { books.search('alan p').should have_categories(['title', 'author'], ['author', 'title']) }
-  
+
 end
