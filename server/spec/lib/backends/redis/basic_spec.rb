382e18089a9df90d69c9cfa623af72d683915c1b
Florian Hanke <florian.hanke@gmail.com>
2011-11-26 00:43:05 +1100

    + specs

--- server/spec/lib/backends/redis/basic_spec.rb
+++ server/spec/lib/backends/redis/basic_spec.rb
@@ -5,39 +5,38 @@ describe Picky::Backends::Redis::Basic do
   let(:client) { stub :client }
 
   context 'without options' do
-    let(:index) { described_class.new client, :some_namespace }
+    let(:backend) { described_class.new client, :some_namespace }
 
     describe 'load, retrieve, delete' do
       it 'is nothing they do (at least on the backend)' do
-        index.should_receive(:client).never
+        backend.should_receive(:client).never
 
-        index.load
-        index.retrieve
-        index.delete
+        backend.load
+        backend.retrieve
       end
     end
 
     describe 'empty' do
-      it 'returns the container that is used for indexing' do
-        index.empty.should == {}
+      it 'returns the container that is used for backending' do
+        backend.empty.should == {}
       end
     end
 
     describe 'initial' do
       it 'is correct' do
-        index.initial.class.should == described_class
+        backend.initial.should == {}
       end
     end
 
     describe 'to_s' do
       it 'returns the cache path with the default file extension' do
-        index.to_s.should == 'Picky::Backends::Redis::Basic(some_namespace:*)'
+        backend.to_s.should == 'Picky::Backends::Redis::Basic(some_namespace:*)'
       end
     end
   end
 
   context 'with options' do
-    let(:index) do
+    let(:backend) do
       described_class.new client,
                           :some_namespace,
                           empty: [],
@@ -45,14 +44,14 @@ describe Picky::Backends::Redis::Basic do
     end
 
     describe 'empty' do
-      it 'returns the container that is used for indexing' do
-        index.empty.should == []
+      it 'returns the container that is used for backending' do
+        backend.empty.should == []
       end
     end
 
     describe 'initial' do
       it 'is correct' do
-        index.initial.class.should == Array
+        backend.initial.class.should == Array
       end
     end
   end
--- server/spec/lib/backends/redis/list_spec.rb
+++ server/spec/lib/backends/redis/list_spec.rb
@@ -1,27 +1,27 @@
 require 'spec_helper'
 
 describe Picky::Backends::Redis::List do
-  
+
   let(:client) { stub :client }
   let(:index) { described_class.new client, :some_namespace }
-  
+
   describe '[]' do
     it 'returns whatever comes back from the backend' do
-      client.stub! :zrange => :some_lrange_result
-      
-      index[:anything].should == :some_lrange_result
+      client.stub! :zrange => [:some_lrange_result]
+
+      index[:anything].should == [:some_lrange_result]
     end
     it 'calls the right method on the backend' do
       client.should_receive(:zrange).once.with "some_namespace:some_sym", 0, -1
-      
+
       index[:some_sym]
     end
   end
-  
+
   describe 'to_s' do
     it 'returns the cache path with the default file extension' do
       index.to_s.should == 'Picky::Backends::Redis::List(some_namespace:*)'
     end
   end
-  
+
 end
