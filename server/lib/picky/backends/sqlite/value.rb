9169abdc5dacf00a1ee65967bca67fe6d6098fe6
Florian Hanke <florian.hanke@gmail.com>
2011-11-25 18:21:13 +1100

    + use insert or replace

--- server/lib/picky/backends/sqlite/value.rb
+++ server/lib/picky/backends/sqlite/value.rb
@@ -7,13 +7,9 @@ module Picky
       class Value < Basic
 
         def []= key, value
-          encoded_value = Yajl::Encoder.encode value
-
-          if self[key]
-            db.execute 'update key_value set value = (?) where key = (?)', encoded_value, key.to_s
-          else
-            db.execute 'insert into key_value (key, value) values (?,?)', key.to_s, encoded_value
-          end
+          db.execute 'insert or replace into key_value (key, value) values (?,?)',
+                     key.to_s,
+                     Yajl::Encoder.encode(value)
 
           value
         end
