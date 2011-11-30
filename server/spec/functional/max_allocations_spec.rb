f1dc8ae0327c2c8814a8c5c624c72813cb09df7b
Florian Hanke <florian.hanke@gmail.com>
2011-11-30 14:34:58 +1100

    + max allocations speed spec

--- server/spec/functional/max_allocations_spec.rb
+++ server/spec/functional/max_allocations_spec.rb
@@ -30,4 +30,35 @@ describe 'Search#max_allocations' do
     try_again.search('hello world').ids.should == [1]
   end
 
+  it 'gets faster' do
+    index = Picky::Index.new :dynamic_weights do
+      category :text1
+      category :text2
+      category :text3
+      category :text4
+    end
+
+    thing = Struct.new(:id, :text1, :text2, :text3, :text4)
+    index.add thing.new(1, 'hello world', 'hello world', 'hello world', 'hello world')
+    index.add thing.new(2, 'hello world', 'hello world', 'hello world', 'hello world')
+    index.add thing.new(3, 'hello world', 'hello world', 'hello world', 'hello world')
+    index.add thing.new(4, 'hello world', 'hello world', 'hello world', 'hello world')
+    index.add thing.new(5, 'hello world', 'hello world', 'hello world', 'hello world')
+    index.add thing.new(6, 'hello world', 'hello world', 'hello world', 'hello world')
+
+    try = Picky::Search.new index
+
+    threshold = performance_of do
+      try.search('hello world')
+    end
+
+    try_again = Picky::Search.new index do
+      max_allocations 1
+    end
+
+    performance_of do
+      try_again.search('hello world')
+    end.should < (threshold*2/3)
+  end
+
 end
