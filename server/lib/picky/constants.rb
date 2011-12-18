50523ce911ee170cfe64693c36da9dd775b2af86
Florian Hanke <florian.hanke@gmail.com>
2011-12-18 23:37:09 +1100

    + Let's be opinionated and do PICKY_ENVIRONMENT = ENV['PICKY_ENV'] || ENV['RUBY_ENV'] || ENV['RACK_ENV']

--- server/lib/picky/constants.rb
+++ server/lib/picky/constants.rb
@@ -3,7 +3,7 @@
 
 # Use rack's environment for the search engine.
 #
-ENV['PICKY_ENV'] ||= ENV['RACK_ENV']
+ENV['PICKY_ENV'] ||= ENV['RUBY_ENV'] || ENV['RACK_ENV']
 
 PICKY_ENVIRONMENT = ENV['PICKY_ENV'] || 'development' unless defined? PICKY_ENVIRONMENT
 PICKY_ROOT        = Dir.pwd unless defined? PICKY_ROOT
