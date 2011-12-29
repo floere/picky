8706a736fb4eaff5666d81cdcd2cc4c697cefee2
Florian Hanke <florian.hanke@gmail.com>
2011-12-29 16:21:07 +1100

    + multi category

---
+++ recipes/basic/multi_category.rb
@@ -0,0 +1,28 @@
+require File.expand_path '../../../server/lib/picky', __FILE__
+
+# We want to search 2 indexes at once.
+# One index uses a source, the indexes at realtime.
+#
+source = Picky::Index.new :people_from_source do
+  source do
+    [Person.new(1, 'Donald', 'Knuth'),
+     Person.new(2, 'Niklaus', 'Wirth')]
+  end
+  category :first
+  category :last
+end
+source.reindex
+
+realtime = Picky::Index.new :people_with_realtime do
+  category :first
+  category :last
+end
+realtime.replace Person.new(3, 'Donald', 'Worth')
+realtime.replace Person.new(4, 'Peter', 'Niklaus')
+
+people = Picky::Search.new source, realtime # <= We pass the search both indexes.
+
+results = people.search 'donald'
+
+# p results.allocations
+fail __FILE__ unless results.ids == [1, 3]
