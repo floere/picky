740c405b08f6c2eb01d09ae773a2365c2a8c2272
Florian Hanke <florian.hanke@gmail.com>
2011-12-31 01:44:59 +1100

    .

--- server/spec/functional/only_spec.rb
+++ server/spec/functional/only_spec.rb
@@ -31,22 +31,31 @@ describe 'Search#only' do
     try_again.search('text3').ids.should == [1]
     
     try_again.search('text1:text1').ids.should == []
+    try_again.search('text1:text2').ids.should == []
+    try_again.search('text1:text3').ids.should == []
+    
+    try_again.search('text2:text1').ids.should == []
     try_again.search('text2:text2').ids.should == [1]
+    try_again.search('text2:text3').ids.should == []
+    
+    try_again.search('text3:text1').ids.should == []
+    try_again.search('text3:text2').ids.should == []
     try_again.search('text3:text3').ids.should == [1]
     
-    try_again.search('text1,text2,text3:text1').ids.should == []
     try_again.search('text1,text2:text1').ids.should == []
-    try_again.search('text1,text3:text1').ids.should == []
-    try_again.search('text2,text3:text1').ids.should == []
-    
-    try_again.search('text1,text2,text3:text2').ids.should == [1]
     try_again.search('text1,text2:text2').ids.should == [1]
-    try_again.search('text1,text3:text2').ids.should == []
-    try_again.search('text2,text3:text2').ids.should == [1]
-    
-    try_again.search('text1,text2,text3:text3').ids.should == [1]
     try_again.search('text1,text2:text3').ids.should == []
+    
+    try_again.search('text1,text3:text1').ids.should == []
+    try_again.search('text1,text3:text2').ids.should == []
     try_again.search('text1,text3:text3').ids.should == [1]
+    
+    try_again.search('text2,text3:text1').ids.should == []
+    try_again.search('text2,text3:text2').ids.should == [1]
     try_again.search('text2,text3:text3').ids.should == [1]
+    
+    try_again.search('text1,text2,text3:text1').ids.should == []
+    try_again.search('text1,text2,text3:text2').ids.should == [1]
+    try_again.search('text1,text2,text3:text3').ids.should == [1]
   end
 end
