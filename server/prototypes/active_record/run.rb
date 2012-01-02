d55819952bce38b4b279a0eaa51b1bd3bba80134
Florian Hanke <florian.hanke@gmail.com>
2012-01-02 17:34:25 +1100

    + Running the script

--- server/prototypes/active_record/run.rb
+++ server/prototypes/active_record/run.rb
@@ -1,14 +1,22 @@
-puts "Start server with 'thin -p 8080 start' (if you haven't done so yet)"
+puts "Start server with 'cd server; thin -p 8080 start'"
+puts "(if you haven't done so already)"
+puts
 
 require_relative 'model'
-
-model = Model.new 1, 'Picky', 'McNamara'
-model.save
-
-model = Model.new 2, 'Florian', 'Hanke'
-model.save
-
 require 'picky-client'
 
 client = Picky::Client.new path: '/search'
+
+# This will output nothing on the first run,
+# but the server will dump/reload even
+# when stopping/starting.
+#
+p client.search 'surname:mcnama*'
+p client.search 'hanke'
+
+Model.new(1, 'Picky', 'McNamara').save
+Model.new(2, 'Florian', 'Hanke').save
+Model.new(3, 'Tentacles', 'Jellyfish').save
+
+p client.search 'surname:mcnama*'
 p client.search 'hanke'
