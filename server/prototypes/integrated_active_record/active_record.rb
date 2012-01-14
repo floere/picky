de7784bb15768fba38870601b2ecf59a64009ec7
Florian Hanke <florian.hanke@gmail.com>
2012-01-14 20:14:36 +1100

    + split into two modules, + extended example

--- server/prototypes/integrated_active_record/active_record.rb
+++ server/prototypes/integrated_active_record/active_record.rb
@@ -11,55 +11,48 @@ module Picky
   module ActiveRecord
     
     def self.included model
-      model.class.class_eval do
-        define_method :updates_picky do |index|
-          model.after_commit do
-            if destroyed?
-              index.remove self.id
-            else
-              index.replace self
+      model.send :include, Indexing
+      model.send :include, Searching
+    end
+    
+    module Indexing
+      def self.included model
+        model.class.class_eval do
+          
+          define_method :updates_picky do |index_or_index_name = nil|
+            index_or_index_name ||= model.name.tableize
+            index = index_or_index_name.respond_to?(:to_sym) ?
+                    Picky::Indexes[index_or_index_name.to_sym] :
+                    index_or_index_name
+            
+            model.after_commit do
+              if destroyed?
+                index.remove self.id
+              else
+                index.replace self
+              end
             end
           end
-        end
-        define_method :searches_picky do |search|
-          model.class.class_eval do
-            define_method :search do |*args|
-              search.search *args
-            end
-          end
+          
         end
       end
     end
     
-    # def initialize index_name = nil, &definition
-    #   self.class.class_eval do
-    #     define_method :extended do |model|
-    #       index_name = index_name || model.name.tableize
-    #       index_name = index_name.to_sym
-    #       
-    #       definition && definition.call # (index_name)
-    #       
-    #       index = Picky::Indexes[index_name]
-    #       search = Picky::Search.new index
-    #       
-    #       model.class.class_eval do
-    #         define_method :search do |*args|
-    #           search.search *args
-    #         end
-    #       end
-    #       
-    #       model.after_commit do
-    #         if destroyed?
-    #           index.remove self.id
-    #         else
-    #           index.replace self
-    #         end
-    #       end
-    #       
-    #     end
-    #   end
-    # 
-    # end
-    
+    module Searching
+      def self.included model
+        model.class.class_eval do
+          
+          define_method :searches_picky do |search|
+            model.class.class_eval do
+              define_method :search do |*args|
+                search.search *args
+              end
+            end
+          end
+          
+        end
+      end
+    end
+     
   end
 end
--- server/prototypes/integrated_active_record/model.rb
+++ server/prototypes/integrated_active_record/model.rb
@@ -26,6 +26,8 @@ class Model < ActiveRecord::Base
   
   search = Picky::Search.new index
   
-  updates_picky index
+  updates_picky index # Pass in nothing (uses index name), index name, or an index.
   searches_picky search
+  
+  # updates_picky index # Updating multiple indexes.
 end
