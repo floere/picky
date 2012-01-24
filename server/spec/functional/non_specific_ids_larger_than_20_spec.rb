e26ad17f0cdc9ea7db84ff15ee0fde0fccf4b69a
Florian Hanke <florian.hanke@gmail.com>
2012-01-24 22:07:21 +1100

    + Spec for #68 (https://github.com/floere/picky/issues/68)

---
+++ server/spec/functional/non_specific_ids_larger_than_20_spec.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+#
+require 'spec_helper'
+
+describe 'Location search with live updates' do
+
+  it 'works with ids larger than 20 on non-specific search' do
+    data = Picky::Index.new :test do
+      category :titel
+      category :text
+    end
+
+    50.times do |i|
+      data.replace_from id: i, titel: 'japan', text: 'some text on japan' 
+    end
+
+    stuff = Picky::Search.new data
+
+    result = stuff.search 'titel:japan', 50
+    result.total.should == 50
+    result.ids.size.should == 50
+    
+    result = stuff.search 'japan', 50
+    result.total.should == 100
+    result.ids.size.should == 100
+  end
+  
+end
