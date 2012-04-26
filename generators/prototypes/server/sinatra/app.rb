60b713e7b9c15592d988b5a4ebec97459be86cab
Florian Hanke <florian.hanke@gmail.com>
2012-04-26 17:17:02 +1000

    - unnecessary regexp code

--- generators/prototypes/server/sinatra/app.rb
+++ generators/prototypes/server/sinatra/app.rb
@@ -31,9 +31,9 @@ class BookSearch < Sinatra::Application
   #
   books_index = Index.new :books do
     source   { Books.new }
-    indexing removes_characters: /[^\p{L}\p{N}\s\/\-\_\:\"\&\.]/i,
+    indexing removes_characters: /[^\p{L}\p{N}\s\/\-\_\:\"\&\.]/,
              stopwords:          /\b(and|the|of|it|in|for)\b/i,
-             splits_text_on:     /[\s\/\-\_\:\"\&\/]/
+             splits_text_on:     /[\s\/\-\_\:\"\&]/
     category :title,
              similarity: Similarity::DoubleMetaphone.new(3),
              partial: Partial::Substring.new(from: 1) # Default is from: -3.
