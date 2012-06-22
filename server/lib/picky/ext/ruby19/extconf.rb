a07387ff154e65f5e956e9425c4b9f2fe09ff17a
Florian Hanke <florian.hanke@gmail.com>
2012-06-22 10:42:51 +1000

    + implement suggestion by David Lowenfels (see https://groups.google.com/forum/?fromgroups#!topic/picky-ruby/8kuG8FkATgU)

--- server/lib/picky/ext/ruby19/extconf.rb
+++ server/lib/picky/ext/ruby19/extconf.rb
@@ -1,4 +1,6 @@
-puts 'Compiling with Ruby 1.9'
+RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC'] 
+
+puts "Compiling on Ruby 1.9 with CC set to #{RbConfig::MAKEFILE_CONFIG['CC']}."
 require 'mkmf'
 
 abort 'need ruby.h' unless have_header("ruby.h")
