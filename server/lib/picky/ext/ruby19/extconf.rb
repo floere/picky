d03c23d661662f73069eeff8680b8e8a5803692a
Florian Hanke <florian.hanke@gmail.com>
2012-07-05 14:13:01 +1000

    + 4.5.3, + test for the existence of RbConfig before using it when compiling the C code.

--- server/lib/picky/ext/ruby19/extconf.rb
+++ server/lib/picky/ext/ruby19/extconf.rb
@@ -1,6 +1,14 @@
-RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC'] 
+# Information.
+#
+print "Compiling on Ruby 1.9"
+if defined?(RbConfig)
+  RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC'] 
+  print " with CC set to #{RbConfig::MAKEFILE_CONFIG['CC']}"
+end
+puts "."
 
-puts "Compiling on Ruby 1.9 with CC set to #{RbConfig::MAKEFILE_CONFIG['CC']}."
+# Compile.
+#
 require 'mkmf'
 
 abort 'need ruby.h' unless have_header("ruby.h")
--- version.rb
+++ version.rb
@@ -1,3 +1,3 @@
 module Picky
-  VERSION = '4.5.2'
+  VERSION = '4.5.3'
 end
