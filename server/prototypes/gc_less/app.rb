e057cf2578ac7fc74ee93dbb02de60b118e31ec9
Florian Hanke <florian.hanke@gmail.com>
2012-07-16 14:52:50 +1000

    + GC less app

--- server/prototypes/gc_less/app.rb
+++ server/prototypes/gc_less/app.rb
@@ -1,5 +1,17 @@
 require_relative '../../lib/picky'
 
+def count klass
+  "#{klass}: #{ObjectSpace.each_object(klass).inject(0) { |total, _| total = total + 1 }}"
+end
+def counts
+  puts count(Picky::Query::Allocation)
+  puts count(Picky::Query::Allocations)
+  puts count(Picky::Query::Combination)
+  puts count(Picky::Query::Combinations)
+  puts count(Picky::Query::Token)
+  puts count(Picky::Query::Tokens)
+end
+
 data = Picky::Index.new :models do
   category :id
   category :name
@@ -9,16 +21,25 @@ end
 data.replace_from id: 1, name: 'Picky', surname: 'McNamara'
 data.replace_from id: 2, name: 'Florian', surname: 'Hanke'
 data.replace_from id: 3, name: 'Tentacles', surname: 'Jellyfish'
+data.replace_from id: 4, name: 'Tentacles', surname: 'Florian'
+data.replace_from id: 5, name: 'Florian', surname: 'Tentacles'
 
 people = Picky::Search.new data
+counts
 
-p ObjectSpace.each_object(Picky::Query::Allocation).inject(0) { |total, _| total = total + 1 }
+puts people.search 'hanke', 20, 0
+counts
 
-results = people.search 'hanke', 20, 0
+puts people.search 'tentacles florian', 20, 0
+counts
 
-p ObjectSpace.each_object(Picky::Query::Allocation).inject(0) { |total, _| total = total + 1 }
+puts people.search 'tentacles florian', 20, 0
+counts
 
-results = people.search 'hanke', 20, 0
-results = people.search 'hanke', 20, 0
+puts people.search 'tentacles florian tentacles', 20, 0
+counts
 
-p ObjectSpace.each_object(Picky::Query::Allocation).inject(0) { |total, _| total = total + 1 }
+require 'benchmark'
+puts Benchmark.measure {
+  1000.times { people.search 'tentacles florian tentacles', 20, 0 }
+}
