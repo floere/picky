34adcadec8f5b19239cb34ad35e554a9b9c789ba
Florian Hanke <florian.hanke@gmail.com>
2012-07-18 14:06:19 +1000

    + facets draft

---
+++ server/prototypes/facets/app.rb
@@ -0,0 +1,34 @@
+require 'picky'
+
+data = Picky::Index.new :models do
+  category :id
+  category :name
+  category :surname
+end
+
+data.replace_from id: 1, name: 'Picky', surname: 'McNamara'
+data.replace_from id: 2, name: 'Florian', surname: 'Hanke'
+data.replace_from id: 3, name: 'Tentacles', surname: 'Jellyfish'
+data.replace_from id: 4, name: 'Tentacles', surname: 'Florian'
+data.replace_from id: 5, name: 'Florian', surname: 'Tentacles'
+
+# Look facets up in the index.
+#
+puts data[:name].exact.inverted.map { |name, ids|
+  [name, ids.size]
+}
+
+class Picky::Index
+  
+  def facets category
+    self[category].exact.inverted.inject({}) { |result, token_ids|
+      token, ids = token_ids
+      result[token] = ids.size
+      result
+    }
+  end
+  
+end
+
+puts data.facets :name
+puts data.facets :surname
