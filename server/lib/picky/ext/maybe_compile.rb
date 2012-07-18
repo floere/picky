5b147b356d1f526d15a02f9e7a139e4a682aa7fc
Florian Hanke <florian.hanke@gmail.com>
2012-07-18 12:15:04 +1000

    ! clang related issue #81, + better error message in case compiling doesn't work

--- server/lib/picky/ext/maybe_compile.rb
+++ server/lib/picky/ext/maybe_compile.rb
@@ -1,9 +1,31 @@
+failed = 0
+
 begin
   require_relative 'ruby19/performant'
 rescue LoadError
-  require_relative 'ruby19/extconf'
-  Dir.chdir File.expand_path('../ruby19', __FILE__) do
-    %x{ ruby extconf.rb && make }
+  failed += 1
+  
+  # For some reason we need to enter ./ruby19
+  # via external command (see issue #81).
+  #
+  Dir.chdir File.expand_path('..', __FILE__) do
+    puts %x(cd ruby19; ruby extconf.rb; make)
   end
-  retry
+  
+  # Try again.
+  #
+  retry if failed < 2
+  
+  # Give up and inform the user.
+  #
+  puts <<-NOTE
+
+Picky tried to compile its source on your system but failed.
+Please add an issue: https://github.com/floere/picky/issues/
+and copy anything into it that you think is helpful. Thanks!
+
+See related issue: https://github.com/floere/picky/issues/81
+
+NOTE
+  exit 1
 end
