43be164c77f7b6f8373fad7b57c55f685fb20c5b
Florian René Hanke <florian.hanke@gmail.com>
2012-09-06 17:11:25 +0200

    - more model setup code

--- server/prototypes/active_record/model.rb
+++ server/prototypes/active_record/model.rb
@@ -1,18 +1,5 @@
 require_relative '../model_setup'
 
-# Set up model backend.
-#
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => ":memory:"
-)
-ActiveRecord::Schema.define(:version => 0) do
-  create_table :models, :force => true do |t|
-    t.string :name
-    t.string :surname
-  end
-end
-
 # Fake ActiveRecord model.
 #
 class Model < ActiveRecord::Base
