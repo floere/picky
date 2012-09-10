769a3ef0c3bae764a07077d6212e8b7e52b0f7b5
Florian René Hanke <florian.hanke@gmail.com>
2012-09-10 22:00:56 +0200

    + rewrite prototypes

--- server/prototypes/active_record/server/app.rb
+++ server/prototypes/active_record/server/app.rb
@@ -14,22 +14,58 @@ class ExternalDataSearch < Sinatra::Base
   include Picky
   extend Sinatra::IndexActions
   
-  data = Index.new :models do
-    category :id
-    category :name
-    category :surname
+  def initialize
+    at_exit { data.dump }
   end
+  
+  def data
+    @data ||= Index.new :models do
+      category :id
+      category :name
+      category :surname
+    end
+  end
+  
+  def finder
+    @finder ||= Search.new data
+  end
+  
   post %r{\A/update\z} do # PUT?
     hash = JSON.parse params['data']
     data.replace_from hash
     puts "UPDATING from #{hash}"
   end
-  at_exit { data.dump }
   
-  models = Search.new data
   get %r{\A/search\z} do
-    results = models.search params['query'], params['ids'] || 20, params['offset'] || 0
+    results = finder.search params['query'], params['ids'] || 20, params['offset'] || 0
     results.to_json
   end
 
 end
+
+# Was this code below, but people don't get contexts etc.
+#
+# class ExternalDataSearch < Sinatra::Base
+# 
+#   include Picky
+#   extend Sinatra::IndexActions
+#   
+#   data = Index.new :models do
+#     category :id
+#     category :name
+#     category :surname
+#   end
+#   post %r{\A/update\z} do # PUT?
+#     hash = JSON.parse params['data']
+#     data.replace_from hash
+#     puts "UPDATING from #{hash}"
+#   end
+#   at_exit { data.dump }
+#   
+#   models = Search.new data
+#   get %r{\A/search\z} do
+#     results = models.search params['query'], params['ids'] || 20, params['offset'] || 0
+#     results.to_json
+#   end
+# 
+# end
