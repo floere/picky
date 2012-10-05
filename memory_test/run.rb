8249c1d3712d3f854c9a2a6a6852096d450f2e78
Florian René Hanke <florian.hanke@gmail.com>
2012-10-05 18:09:21 +1000

    + memory tests

--- memory_test/run.rb
+++ memory_test/run.rb
@@ -3,10 +3,13 @@ require_relative 'definition'
 
 # For Pickies that don't have that baked into.
 #
-# MultiJson.use :yajl if defined? MultiJson
+MultiJson.use :yajl if defined? MultiJson
+
+puts ENV['PICKY_VERSION'] || 'Latest version (pid, rss, command)'
 
 Picky::Indexes.load
 
 # p Picky::Search.new(Picky::Indexes[:memory]).search 'rfrdpmahxrruqzqrhszipsfltrspyq'
 
-puts `ps -e -www -o pid,rss,command | grep 'ruby run.rb'`
+puts `ps -e -www -o pid,rss,command | grep 'ruby \\brun.rb'`
+puts
