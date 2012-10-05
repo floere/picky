b808fadf0b4c4686a585ed5829292e128ddc06ca
Florian René Hanke <florian.hanke@gmail.com>
2012-10-05 14:57:37 +1000

    + use yajl

--- memory_test/run.rb
+++ memory_test/run.rb
@@ -1,6 +1,10 @@
 Bundler.require
 require_relative 'definition'
 
+# For Pickies that don't have that baked into.
+#
+MultiJson.use :yajl if defined?(MultiJson)
+
 Picky::Indexes.load
 
 puts `ps -e -www -o pid,rss,command | grep 'ruby run.rb'`
