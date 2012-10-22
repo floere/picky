49dc696c8feb70871d8f000a2bafe8f698a680cf
Florian René Hanke <hanke@soyuz.local>
2012-10-22 13:58:41 +1100

    - TODO

--- server/spec/lib/picky_spec.rb
+++ server/spec/lib/picky_spec.rb
@@ -5,7 +5,7 @@ describe Picky do
   it 'sets the right internal encoding' do
     Encoding.default_external.should == Encoding::UTF_8
   end
-  # TODO Set default external encoding.
+  # THINK What to set default external encoding to?
   #
   # it 'sets the right external encoding' do
   #   Encoding.default_internal.should == Encoding::UTF_8
