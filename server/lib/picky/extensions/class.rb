b73a9647d1a5cba2bf9b981ccb9948030ff8f434
Florian René Hanke <hanke@soyuz.local>
2012-10-22 13:54:59 +1100

    + make delegation more straightforward

--- server/lib/picky/extensions/class.rb
+++ server/lib/picky/extensions/class.rb
@@ -2,9 +2,11 @@
 #
 class Class
 
-  def instance_delegate *methods
-    methods.each do |method|
-      module_eval("def self.#{method}(*args, &block)\nself.instance.__send__(#{method.inspect}, *args, &block)\nend\n", "(__DELEGATION__)", 1)
+  def instance_delegate *method_names
+    method_names.each do |method_name|
+      module_eval(<<-DELEGATION, "(__DELEGATION__)", 1)
+def self.#{method_name}(*args, &block)\n  self.instance.#{method_name}(*args, &block)\nend
+DELEGATION
     end
   end
 
