4f5cb8c15b8359ac36e3a967261ea0af85919f81
Florian René Hanke <florian.hanke@gmail.com>
2012-10-28 03:24:50 +1100

    + publish.sh taken from kschiess/parslet

---
+++ web/publish.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+bundle exec middleman build --verbose 
+# doc_sha=$(git ls-tree -d HEAD build | awk '{print $3}')
+# git cat-file -p $doc_sha
+# new_commit=$(echo "Auto-update docs." | git commit-tree $doc_sha -p refs/heads/gh-pages)
+# git update-ref refs/heads/gh-pages $new_commit
