7cb409759ef7558d9165edd61671be3c86cedcb4
Florian René Hanke <florian.hanke@gmail.com>
2012-11-06 11:12:34 +1100

    + range character can be customized

--- server/spec/functional/custom_delimiters_spec.rb
+++ server/spec/functional/custom_delimiters_spec.rb
@@ -4,11 +4,14 @@ require 'spec_helper'
 
 describe 'custom delimiters' do
   
+  # TODO Add range query delimiter.
+  #
   after(:each) do
     Picky::Query::Token.partial_character = '\*'
     Picky::Query::Token.no_partial_character = '"'
     Picky::Query::Token.similar_character = '~'
     Picky::Query::Token.no_similar_character = '"'
+    Picky::Query::Token.range_character = '-'
     Picky::Query::Token.qualifier_text_delimiter = ':'
     Picky::Query::Token.qualifiers_delimiter = ','
   end
@@ -75,5 +78,26 @@ describe 'custom delimiters' do
     try.search("text1|text2?hello text2?world").ids.should == []
     Picky::Query::Token.qualifiers_delimiter = '|'
     try.search("text1|text2?hello text2?world").ids.should == [1]
+  end
+  
+  it 'offers custom range characters to be set' do
+    index = Picky::Index.new :custom_range_character do
+      category :year
+    end
+
+    rangy = Struct.new :id, :year
+    
+    index.add rangy.new(1, 1977)
+    index.add rangy.new(2, 1989)
+    index.add rangy.new(3, 2001)
+    index.add rangy.new(4, 2012)
+    index.add rangy.new(4, 3000)
+
+    try = Picky::Search.new index
+    try.search("1980-2015").ids.should == [2,3,4]
+    
+    try.search("1980…2015").ids.should == []
+    Picky::Query::Token.range_character = "…"
+    try.search("1980…2015").ids.should == [2,3,4]
   end
 end
