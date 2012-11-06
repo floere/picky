19005be50257d14e844fa6e1ee5538f1e609ca7a
Florian René Hanke <florian.hanke@gmail.com>
2012-11-06 11:11:32 +1100

    + Token#rangify performance tests

---
+++ server/prototypes/range_query/performance.rb
@@ -0,0 +1,45 @@
+exc = 'desoxyribonucleinacid'
+inc = '2000-2008'
+
+# include? seems to be fastest.
+#
+t = Time.now
+1000.times do
+  inc =~ /\-/
+end
+p [:inc, :'=~', (Time.now - t)]
+t = Time.now
+1000.times do
+  inc.include? '-'
+end
+p [:inc, :include?, (Time.now - t)]
+
+t = Time.now
+1000.times do
+  exc =~ /\-/
+end
+p [:exc, :'=~', (Time.now - t)]
+t = Time.now
+1000.times do
+  exc.include? '-'
+end
+p [:exc, :include?, (Time.now - t)]
+
+
+# Splitting the text should only split on the first.
+#
+
+raise if "a-b-c".split('-', 2) != ['a', 'b-c']
+
+s = "a-b-c"
+
+t = Time.now
+1000.times do
+  s.split('-')
+end
+p ['-', (Time.now - t)]
+t = Time.now
+1000.times do
+  s.split('-', 2)
+end
+p ['-, 2', (Time.now - t)]
