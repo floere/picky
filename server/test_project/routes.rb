137782d5106a435bb048cdbcaee4358ea5637322
Florian René Hanke <florian.hanke@gmail.com>
2012-12-03 16:59:26 +1100

    .

--- server/test_project/routes.rb
+++ server/test_project/routes.rb
@@ -1,9 +1,7 @@
 # encoding: utf-8
 #
 
-class BookSearch
-  
-  include Picky
+BookSearch.instance_eval do
   
   weights = {
     [:author]         => +6,
@@ -11,42 +9,43 @@ class BookSearch
     [:author, :year]  => +2
   }
 
-  map 'books', Search.new(BooksIndex, ISBNIndex) {
+  map 'books', Picky::Search.new(BooksIndex, ISBNIndex) {
     boost weights
   }
-  map 'books_ignoring', Search.new(BooksIndex, ISBNIndex) {
+  map 'books_ignoring', Picky::Search.new(BooksIndex, ISBNIndex) {
     boost weights
     ignore_unassigned_tokens true
   }
-  map 'book_each', Search.new(BookEachIndex) {
+  map 'book_each', Picky::Search.new(BookEachIndex) {
     boost weights
     # ignore :title
   }
-  map 'redis', Search.new(RedisIndex) {
+  map 'redis', Picky::Search.new(RedisIndex) {
     boost weights
   }
-  map 'memory_changing', Search.new(MemoryChangingIndex)
-  map 'redis_changing', Search.new(RedisChangingIndex)
-  map 'csv', Search.new(CSVTestIndex) {
+  map 'memory_changing', Picky::Search.new(MemoryChangingIndex)
+  map 'redis_changing', Picky::Search.new(RedisChangingIndex)
+  map 'csv', Picky::Search.new(CSVTestIndex) {
     boost weights
   }  
-  map 'isbn', Search.new(ISBNIndex)
-  map 'sym', Search.new(SymKeysIndex)
-  map 'geo', Search.new(RealGeoIndex)
-  map 'simple_geo', Search.new(MgeoIndex)
-  map 'iphone', Search.new(IphoneLocations)
-  map 'indexing', Search.new(IndexingIndex)
-  map 'file', Search.new(FileIndex)
-  map 'japanese', Search.new(JapaneseIndex) {
+  map 'isbn', Picky::Search.new(ISBNIndex)
+  map 'sym', Picky::Search.new(SymKeysIndex)
+  map 'geo', Picky::Search.new(RealGeoIndex)
+  map 'simple_geo', Picky::Search.new(MgeoIndex)
+  map 'iphone', Picky::Search.new(IphoneLocations)
+  map 'indexing', Picky::Search.new(IndexingIndex)
+  map 'file', Picky::Search.new(FileIndex)
+  map 'japanese', Picky::Search.new(JapaneseIndex) {
     searching removes_characters: /[^\p{Han}\p{Katakana}\p{Hiragana}\"\~\*\:\,]/i, # a-zA-Z0-9\s\/\-\_\&\.
               stopwords:          /\b(and|the|of|it|in|for)\b/i,
               splits_text_on:     /[\s\/\-\&]+/
   }
-  map 'nonstring', Search.new(NonstringDataIndex)
-  map 'partial', Search.new(PartialIndex)
-  # map 'sqlite', Search.new(SQLiteIndex) # TODO Fix, reinstate.
-  map 'commas', Search.new(CommaIdsIndex)
-  map 'all', Search.new(BooksIndex, CSVTestIndex, ISBNIndex, MgeoIndex) {
+  map 'nonstring', Picky::Search.new(NonstringDataIndex)
+  map 'partial', Picky::Search.new(PartialIndex)
+  # map 'sqlite', Picky::Search.new(SQLiteIndex) # TODO Fix, reinstate.
+  map 'commas', Picky::Search.new(CommaIdsIndex)
+  map 'all', Picky::Search.new(BooksIndex, CSVTestIndex, ISBNIndex, MgeoIndex) {
     boost weights
   }
+  
 end
