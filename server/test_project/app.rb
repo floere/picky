b509e0569328e4d9c16d2372c40533c059a283eb
Florian René Hanke <florian.hanke@gmail.com>
2012-12-03 17:16:46 +1100

    + Hash the routes

--- server/test_project/app.rb
+++ server/test_project/app.rb
@@ -22,57 +22,56 @@ require_relative 'defaults'
 
 class BookSearch < Sinatra::Application
   
-  def self.map url, things
-    self.get %r{\A/#{url}\z} do
-      things.search(params[:query], params[:ids] || 20, params[:offset] || 0).to_json
-    end
-  end
-  
-  def self.install_routes
-    weights = {
-      [:author]         => +6,
-      [:title, :author] => +5,
-      [:author, :year]  => +2
-    }
+  weights = {
+    [:author]         => +6,
+    [:title, :author] => +5,
+    [:author, :year]  => +2
+  }
 
-    map 'books', Picky::Search.new(BooksIndex, ISBNIndex) {
+  routes = {
+    books: Picky::Search.new(BooksIndex, ISBNIndex) {
       boost weights
-    }
-    map 'books_ignoring', Picky::Search.new(BooksIndex, ISBNIndex) {
+    },
+    books_ignoring: Picky::Search.new(BooksIndex, ISBNIndex) {
       boost weights
       ignore_unassigned_tokens true
-    }
-    map 'book_each', Picky::Search.new(BookEachIndex) {
+    },
+    book_each: Picky::Search.new(BookEachIndex) {
       boost weights
       # ignore :title
-    }
-    map 'redis', Picky::Search.new(RedisIndex) {
+    },
+    redis: Picky::Search.new(RedisIndex) {
       boost weights
-    }
-    map 'memory_changing', Picky::Search.new(MemoryChangingIndex)
-    map 'redis_changing', Picky::Search.new(RedisChangingIndex)
-    map 'csv', Picky::Search.new(CSVTestIndex) {
+    },
+    memory_changing: Picky::Search.new(MemoryChangingIndex),
+    redis_changing: Picky::Search.new(RedisChangingIndex),
+    csv: Picky::Search.new(CSVTestIndex) {
       boost weights
-    }  
-    map 'isbn', Picky::Search.new(ISBNIndex)
-    map 'sym', Picky::Search.new(SymKeysIndex)
-    map 'geo', Picky::Search.new(RealGeoIndex)
-    map 'simple_geo', Picky::Search.new(MgeoIndex)
-    map 'iphone', Picky::Search.new(IphoneLocations)
-    map 'indexing', Picky::Search.new(IndexingIndex)
-    map 'file', Picky::Search.new(FileIndex)
-    map 'japanese', Picky::Search.new(JapaneseIndex) {
+    },
+    isbn: Picky::Search.new(ISBNIndex),
+    sym: Picky::Search.new(SymKeysIndex),
+    geo: Picky::Search.new(RealGeoIndex),
+    simple_geo: Picky::Search.new(MgeoIndex),
+    iphone: Picky::Search.new(IphoneLocations),
+    indexing: Picky::Search.new(IndexingIndex),
+    file: Picky::Search.new(FileIndex),
+    japanese: Picky::Search.new(JapaneseIndex) {
       searching removes_characters: /[^\p{Han}\p{Katakana}\p{Hiragana}\"\~\*\:\,]/i, # a-zA-Z0-9\s\/\-\_\&\.
                 stopwords:          /\b(and|the|of|it|in|for)\b/i,
                 splits_text_on:     /[\s\/\-\&]+/
-    }
-    map 'nonstring', Picky::Search.new(NonstringDataIndex)
-    map 'partial', Picky::Search.new(PartialIndex)
-    # map 'sqlite', Picky::Search.new(SQLiteIndex) # TODO Fix, reinstate.
-    map 'commas', Picky::Search.new(CommaIdsIndex)
-    map 'all', Picky::Search.new(BooksIndex, CSVTestIndex, ISBNIndex, MgeoIndex) {
+    },
+    nonstring: Picky::Search.new(NonstringDataIndex),
+    partial: Picky::Search.new(PartialIndex),
+    # sqlite: Picky::Search.new(SQLiteIndex), # TODO Fix, reinstate.
+    commas: Picky::Search.new(CommaIdsIndex),
+    all: Picky::Search.new(BooksIndex, CSVTestIndex, ISBNIndex, MgeoIndex) {
       boost weights
     }
+  }
+  routes.each do |(path, things)|
+    get %r{\A/#{path}\z} do
+      things.search(params[:query], params[:ids] || 20, params[:offset] || 0).to_json
+    end
   end
 
   # Live.
@@ -84,5 +83,3 @@ class BookSearch < Sinatra::Application
   end
 
 end
-
-BookSearch.install_routes
