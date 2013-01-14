3ad44393aa7b7d29349eaa7ebdb0d00c8379222d
Florian René Hanke <florian.hanke@gmail.com>
2013-01-14 17:52:55 +1100

    + TODO

--- server/lib/picky/indexes.rb
+++ server/lib/picky/indexes.rb
@@ -47,7 +47,9 @@ module Picky
     # Registers an index with the indexes.
     #
     def register index
-      self.indexes.delete_if { |existing| existing.name == index.name }
+      # TODO DO not store duplicate indexes.
+      #
+      # self.indexes.delete_if { |existing| existing.name == index.name }
       self.indexes << index
       self.index_mapping[index.name] = index
     end
