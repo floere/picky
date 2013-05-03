73ba80d9a26b5e8a78aae7326f11d27642902076
Florian René Hanke <florian.hanke@gmail.com>
2013-05-04 03:10:45 +1000

    + Refactor cardinality method

--- server/lib/picky/analyzer.rb
+++ server/lib/picky/analyzer.rb
@@ -1,6 +1,17 @@
 # encoding: utf-8
 #
 
+class Range
+  def expand_with thing
+    return (thing..thing) unless min
+    if max < thing
+      (min..thing)
+    else
+      thing < min ? (thing..max) : self
+    end
+  end
+end
+
 # Analyzes indexes (index bundles, actually).
 #
 class Analyzer
@@ -38,47 +49,36 @@ class Analyzer
 
     self
   end
+    
   def cardinality identifier, index
     return unless can_calculate_cardinality? index
 
     key_length_sum = 0
     ids_length_sum = 0
+    
+    key_length = (1.0/0..0)
+    ids_length = (1.0/0..0)
 
-    min_ids_length = 1.0/0 # Infinity
-    max_ids_length =     0
-    min_key_length = 1.0/0 # Infinity
-    max_key_length =     0
-
-    key_size, ids_size = 0, 0
     index.each_pair do |key, ids|
-      key_size = key.size
-      if key_size < min_key_length
-        min_key_length = key_size
-      else
-        max_key_length = key_size if key_size > max_key_length
-      end
-      key_length_sum += key_size
-
-      ids_size = ids.size
-      if ids_size < min_ids_length
-        min_ids_length = ids_size
-      else
-        max_ids_length = ids_size if ids_size > max_ids_length
-      end
-      ids_length_sum += ids_size
+      key_length = key_length.expand_with key.size
+      key_length_sum += key.size
+      ids_length = ids_length.expand_with ids.size
+      ids_length_sum += ids.size
     end
 
     analysis[identifier] ||= {}
-    analysis[identifier][:key_length]         = (min_key_length..max_key_length)
-    analysis[identifier][:ids_length]         = (min_ids_length..max_ids_length)
+    analysis[identifier][:key_length]         = key_length
+    analysis[identifier][:ids_length]         = ids_length
     analysis[identifier][:key_length_average] = key_length_sum.to_f / index.size
     analysis[identifier][:ids_length_average] = ids_length_sum.to_f / index.size
   end
+  
   def can_calculate_cardinality? index
     return if index.size.zero?
     return unless index.respond_to? :each_pair
     true
   end
+  
   def index_analysis
     return unless analysis[:index]
 
@@ -92,6 +92,7 @@ class Analyzer
       comments << "\033[33mIndex matches single characters.\033[m"
     end
   end
+  
   def weights index
     return if !index.respond_to?(:size) || index.size.zero?
     return unless index.respond_to?(:each_pair)
@@ -116,6 +117,7 @@ class Analyzer
     analysis[:weights][:weight_range]   = (min_weight..max_weight)
     analysis[:weights][:weight_average] = weight_average
   end
+  
   def weights_analysis
     return unless analysis[:weights]
 
@@ -132,6 +134,7 @@ class Analyzer
   def to_s
     [*comments, index_to_s, weights_to_s, similarity_to_s, configuration_to_s].compact.join "\n"
   end
+  
   def index_to_s
     return if analysis[:__keys].zero?
     ary = ["index key cardinality:                #{"%9d" % analysis[:__keys]}"]
@@ -140,12 +143,14 @@ class Analyzer
     ary << formatted('ids per', :ids_length)
     ary.join "\n"
   end
+  
   def formatted description, key, index = :index
     what    = "%-40s" % ["index", description, "key length range (avg):"].compact.join(' ')
     range   = "%7s" % analysis[index][key]
     average = "%8s" % "(#{analysis[index][:"#{key}_average"].round(2)})"
     what + range + average
   end
+  
   def weights_to_s
     return unless analysis[:weights]
     what    = "%-30s" % "weights range (avg):"
@@ -153,10 +158,12 @@ class Analyzer
     average = "%8s" % "(#{analysis[:weights][:weight_average].round(2)})"
     what + range + average
   end
+  
   def similarity_to_s
     return unless analysis[:similarity]
     formatted('similarity', :key_length, :similarity)
   end
+  
   def configuration_to_s
     # analysis[:configuration]
   end
