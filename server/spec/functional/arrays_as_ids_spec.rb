6faddb4c4091646896bb2e4d58d9b4789de1c307
Florian René Hanke <florian.hanke@gmail.com>
2013-06-03 00:09:02 +1000

    + String#split as key_format spec.

--- server/spec/functional/arrays_as_ids_spec.rb
+++ server/spec/functional/arrays_as_ids_spec.rb
@@ -29,4 +29,29 @@ describe "Array IDs" do
     ] # WAT
   end
 
+  # This tests the weights option.
+  #
+  it 'can use split as key_format' do
+    index = Picky::Index.new :arrays do
+      key_format :split
+      
+      category :text1
+    end
+
+    require 'ostruct'
+    
+    thing = OpenStruct.new id: "id1 thing1", text1: "ohai"
+    other = OpenStruct.new id: "id2 thing2", text1: "ohai kthxbye"
+
+    index.add thing
+    index.add other
+
+    try = Picky::Search.new index
+
+    try.search("text1:ohai").ids.should == [
+      ["id2", "thing2"],
+      ["id1", "thing1"]
+    ] # WAT
+  end
+
 end
