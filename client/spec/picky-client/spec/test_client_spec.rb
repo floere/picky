bbbc75aa1a067392753bc6d599f16970d2c467d4
Florian R. Hanke <florian.hanke@gmail.com>
2013-12-01 12:58:10 +0100

    + Test for ids param.

--- client/spec/picky-client/spec/test_client_spec.rb
+++ client/spec/picky-client/spec/test_client_spec.rb
@@ -20,6 +20,16 @@ describe Picky::TestClient do
       
       client.search('test').total.should == 1
     end
+    it 'sends correctly' do
+      response = stub :response
+      request = stub :request
+      client.stub! :request => request
+      
+      request.should_receive(:get).once.with('/some/path?ids=100&query=some_query').and_return response
+      response.should_receive(:body).once.and_return '"some_body"'
+      
+      client.search('some_query', :ids => 100).should == 'some_body'
+    end
   end
   
   context 'send_search' do
