14ee4956ee63c9d14878166759d7cdcd7b312a42
Florian R. Hanke <florian.hanke@gmail.com>
2014-05-03 17:43:27 +0200

    ! Spec.

--- server/spec/functional/terminate_early_spec.rb
+++ server/spec/functional/terminate_early_spec.rb
@@ -126,27 +126,23 @@ describe 'Search#terminate_early' do
     try.search('hello', 1, 20).allocations.map(&:count).should == [6, 6, 6, 6]
     try.search('hello', 1, 24).allocations.map(&:count).should == [6, 6, 6, 6]
 
-    try.search('hello', 1, 0).to_hash.should == {
-      :allocations => [
+    result_hash = try.search('hello', 1, 0).to_hash
+    result_hash[:allocations].should == [
         [:terminate_early, 1.792, 6, [[:text1, "hello", "hello"]], [6]],
         [:terminate_early, 1.792, 6, [[:text2, "hello", "hello"]], []]
-      ],
-      :offset => 0,
-      :duration => anything,
-      :total => 12
-    }
+      ]
+    result_hash[:offset].should == 0
+    result_hash[:total].should == 12
 
-    try.search('hello', 1, 12).to_hash.should == {
-      :allocations => [
+    result_hash = try.search('hello', 1, 12).to_hash
+    result_hash[:allocations].should == [
         [:terminate_early, 1.792, 6, [[:text1, "hello", "hello"]], []],
         [:terminate_early, 1.792, 6, [[:text2, "hello", "hello"]], []],
         [:terminate_early, 1.792, 6, [[:text3, "hello", "hello"]], [6]],
         [:terminate_early, 1.792, 6, [[:text4, "hello", "hello"]], []]
-      ],
-      :offset => 12,
-      :duration => anything,
-      :total => 24
-    }
+      ]
+    result_hash[:offset].should == 12
+    result_hash[:total].should == 24
 
     GC.start
 
