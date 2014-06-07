6220f826bb85df1cb016e7ec65371178dae9a3f9
Florian R. Hanke <florian.hanke@gmail.com>
2014-06-07 21:36:56 +0200

    + Functional spec for similarity.

---
+++ server/spec/functional/similarity_spec.rb
@@ -0,0 +1,72 @@
+# encoding: utf-8
+#
+require 'spec_helper'
+
+describe 'similarity' do
+  it 'does Soundex' do
+    index = Picky::Index.new :phonetic do
+      indexing removes_characters: /[^a-z\s]/i
+      category :text, similarity: Picky::Similarity::Soundex.new(1)
+    end
+
+    index.replace_from id: 1, text: "Peter"
+
+    try = Picky::Search.new index
+
+    # No ~, no similarity.
+    #
+    try.search("text:petor").ids.should == []
+
+    # Finds soundex-similar text.
+    #
+    try.search("text:petor~").ids.should == [1]
+
+    # Finds the identity.
+    #
+    try.search("text:peter~").ids.should == [1]
+  end
+  it 'does Metaphone' do
+    index = Picky::Index.new :phonetic do
+      indexing removes_characters: /[^a-z\s]/i
+      category :text, similarity: Picky::Similarity::Metaphone.new(1)
+    end
+
+    index.replace_from id: 1, text: "Peter"
+
+    try = Picky::Search.new index
+
+    # No ~, no similarity.
+    #
+    try.search("text:pdr").ids.should == []
+
+    # Finds soundex-similar text.
+    #
+    try.search("text:pdr~").ids.should == [1]
+
+    # Finds the identity.
+    #
+    try.search("text:peter~").ids.should == [1]
+  end
+  it 'does DoubleMetaphone' do
+    index = Picky::Index.new :phonetic do
+      indexing removes_characters: /[^a-z\s]/i
+      category :text, similarity: Picky::Similarity::DoubleMetaphone.new(1)
+    end
+
+    index.replace_from id: 1, text: "Peter"
+
+    try = Picky::Search.new index
+
+    # No ~, no similarity.
+    #
+    try.search("text:pdr").ids.should == []
+
+    # Finds soundex-similar text.
+    #
+    try.search("text:pdr~").ids.should == [1]
+
+    # Finds the identity.
+    #
+    try.search("text:peter~").ids.should == [1]
+  end
+end
