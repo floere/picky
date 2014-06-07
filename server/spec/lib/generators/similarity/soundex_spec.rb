d0917256546d1df93fe58c18177bb0b00631c58d
Florian R. Hanke <florian.hanke@gmail.com>
2014-06-07 21:20:54 +0200

    - Outdated specs.

--- server/spec/lib/generators/similarity/soundex_spec.rb
+++ server/spec/lib/generators/similarity/soundex_spec.rb
@@ -20,48 +20,8 @@ describe Picky::Generators::Similarity::Soundex do
     end
   end
 
-  # def self.it_should_generate_from index, expected
-  #   it "should generate #{expected.inspect} correctly from #{index.inspect}" do
-  #     @similarity.generate_from(index).should == expected
-  #   end
-  # end
-
   it_should_encode :meier,       :M600
   it_should_encode :grossberger, :G621
   it_should_encode :hadelbla,    :H341
-
-  # it_should_generate_from({}, {})
-  # it_should_generate_from({ :maier => nil, :meier => nil }, :M600 => [:maier, :meier]) # should be correctly ordered
-  # it_should_generate_from({ :maier => nil, :meier => nil, :hallaballa => nil }, :M600 => [:maier, :meier], :H414 => [:hallaballa])
-  # it_should_generate_from({ :susan => nil, :susanne => nil, :bruderer => nil }, :S250 => [:susan, :susanne], :B636 => [:bruderer])
-
-  # describe 'with reduced amount' do
-  #   before(:each) do
-  #     @similarity = described_class.new(1)
-  #   end
-  #   it_should_generate_from({ :maier => nil, :meier => nil }, :M600 => [:maier])
-  #   it_should_generate_from({ :susan => nil, :susanne => nil, :bruderer => nil }, :S250 => [:susan], :B636 => [:bruderer])
-  # end
-
-  # describe 'hashify' do
-  #   it 'should turn an empty list into an empty hash' do
-  #     @similarity.send(:hashify, []).should == {}
-  #   end
-  #   it 'should turn the list into an unordered similarity' do
-  #     @similarity.send(:hashify, [:meier, :maier]).should == { :M600 => [:meier, :maier] }
-  #   end
-  #   it 'should turn the list into a encoded hash' do
-  #     @similarity.send(:hashify, [:meier, :maier, :peter]).should == { :M600 => [:meier, :maier], :P360 => [:peter] }
-  #   end
-  # end
-
-  # context 'integration' do
-  #   it 'should return the right ordered array' do
-  #     index = @similarity.generate_from :meier => nil, :maier => nil, :mairai => nil, :mair => nil, :meira => nil
-  #     code  = @similarity.encoded :maier
-  #
-  #     index[code].should == [:mair, :maier, :meier, :meira, :mairai]
-  #   end
-  # end
 
 end
