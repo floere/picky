65a21c0c743f43db9fe13000a1f2be235e47514c
Florian R. Hanke <florian.hanke@gmail.com>
2014-11-13 13:56:46 +0100

    Generated webpage.

--- web/config.rb
+++ web/config.rb
@@ -93,10 +93,10 @@ set :haml, { ugly: true }
 #
 configure :build do
   # For example, change the Compass output style for deployment
-  activate :minify_css
+  # activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript
+  # activate :minify_javascript
 
   # Enable cache buster
   # activate :cache_buster
