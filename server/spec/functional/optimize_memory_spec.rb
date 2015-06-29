b973d39a489b5ac88ba2d2c0d52f7136fee4de26
Florian R. Hanke <florian.hanke@gmail.com>
2015-06-29 19:19:45 +0200

    Optimizing can help with Array memory usage.

--- server/spec/functional/optimize_memory_spec.rb
+++ server/spec/functional/optimize_memory_spec.rb
@@ -4,20 +4,23 @@ require 'spec_helper'
 
 describe "Memory optimization" do
   
-  it 'saves memory' do
+  before(:each) do
     # Remove all indexes.
     Picky::Indexes.clear_indexes
     
-    index = Picky::Index.new :memory_optimization do
+    @index = Picky::Index.new :memory_optimization do
       category :text1
       category :text2
       category :text3
       category :text4
     end
-    try = Picky::Search.new index
     
-    thing = Struct.new(:id, :text1, :text2, :text3, :text4)
-    
+    @thing = Struct.new(:id, :text1, :text2, :text3, :text4)
+  end
+  
+  attr_reader :index, :thing
+  
+  it 'saves memory' do
     require 'objspace'
     
     GC.start
@@ -38,6 +41,9 @@ describe "Memory optimization" do
     memsize_with_optimized_memory = ObjectSpace.memsize_of_all(Array)
     GC.start
     
+    # Still larger than with nothing.
+    memsize_without_added_thing.should < memsize_with_optimized_memory
+    # But smaller than with added.
     memsize_with_optimized_memory.should < memsize_with_added_thing
   end
 
