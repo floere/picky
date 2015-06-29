883c2d1fe33134cc1599b1b5f9bcb89ea6ea5579
Florian R. Hanke <florian.hanke@gmail.com>
2015-06-29 19:19:38 +0200

    Does not leak.

--- server/spec/functional/memory_leak_spec.rb
+++ server/spec/functional/memory_leak_spec.rb
@@ -4,20 +4,23 @@ require 'spec_helper'
 
 describe "Memory leak?" do
   
-  it 'leaks a little' do
+  before(:each) do
     # Remove all indexes.
     Picky::Indexes.clear_indexes
     
-    index = Picky::Index.new :memory_leak do
+    @index = Picky::Index.new :memory_optimization do
       category :text1
       category :text2
       category :text3
       category :text4
     end
-    try = Picky::Search.new index
     
-    thing = Struct.new(:id, :text1, :text2, :text3, :text4)
-    
+    @thing = Struct.new(:id, :text1, :text2, :text3, :text4)
+  end
+  
+  attr_reader :index, :thing
+  
+  it 'does not leak' do
     require 'objspace'
     
     GC.start
@@ -25,22 +28,18 @@ describe "Memory leak?" do
     GC.start
     
     index.add thing.new(1, 'one', 'two', 'three', 'four')
-    GC.start
     
     GC.start
     memsize_with_added_thing = ObjectSpace.memsize_of_all(Array)
     GC.start
     
-    memsize_with_added_thing.should > memsize_without_added_thing
-    
-    GC.start
     index.add thing.new(1, 'one', 'two', 'three', 'four')
     
     GC.start
-    memsize_with_reindexed_thing = ObjectSpace.memsize_of_all(Array)
+    memsize_with_readded_thing = ObjectSpace.memsize_of_all(Array)
     GC.start
     
-    memsize_with_reindexed_thing.should == memsize_with_added_thing
+    memsize_with_readded_thing.should == memsize_with_added_thing
   end
 
 end
