4ee23ce952ad3aae3b35d6618796835a293d1757
Florian R. Hanke <florian.hanke@gmail.com>
2015-09-09 14:05:29 +0200

    Start adding a Rust prototype for array i16 intersection.

---
+++ server/prototypes/rust/arrays/src/lib.rs
@@ -0,0 +1,50 @@
+// #![crate_type = "dylib"]
+// extern crate libc;
+pub use libc::types::os::arch::c95::c_char;
+pub use libc::types::os::arch::c99::uintptr_t;
+
+// A simple array-like thing that holds i16 numbers.
+//
+
+#[no_mangle]
+pub extern "C" fn append(other: i16) {
+  
+}
+
+#[no_mangle]
+pub extern "C" fn intersect(other: Vec<i16>) -> Vec<i16> {
+  
+}
+
+#[link(name = "ruby")]
+extern {
+  unsafe rb_cObject: libc::uintptr_t;
+  fn rb_define_module(name: *const libc::c_char) -> libc::uintptr_t;
+  fn rb_define_class_under(
+    module: libc::uintptr_t,
+    name: *const libc::c_char,
+    klass: libc::uintptr_t
+  );
+  fn rb_define_method(
+    klass: libc::uintptr_t,
+    name: *const libc::c_char,
+    function: extern fn(libc::uintptr_t),
+    argc: libc::c_int
+  );
+  fn rb_define_singleton_method(
+    klass: libc::uintptr_t,
+    name: *const libc::c_char,
+    callback: extern fn(libc::uintptr_t),
+    argc: libc::c_int
+  );
+}
+
+#[no_mangle]
+pub extern fn Init_picky() {
+  // Module/Class structure.
+  let rust_module = unsafe { rb_define_module("Rust".to_c_str().as_ptr()) };
+  let rust_array  = unsafe { rb_define_class_under(rust_module, "Array".to_c_str().as_ptr(), rb_cObject) };
+  
+  // Ruby methods.
+  unsafe { rb_define_method(rust_array, "intersect", intersect, 1); }
+}
