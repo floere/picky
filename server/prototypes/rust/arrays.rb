b1dc7c880cbc10f547c1ff539746cc3029570de8
Florian R. Hanke <florian.hanke@gmail.com>
2015-09-09 16:19:17 +0200

    Does not run yet, but close.

--- server/prototypes/rust/arrays.rb
+++
@@ -1,8 +0,0 @@
-require './target/release/arrays'
-
-array1 = Rust::Array.new
-array2 = Rust::Array.new
-
-array1.intersect(array2)
-
-puts 'Yeah!'
---
+++ server/prototypes/rust/arrays/manual_test.rb
@@ -0,0 +1,5 @@
+require File.expand_path('../target/release/libarrays', __FILE__)
+
+# Rust
+
+puts 'Yeah!'
