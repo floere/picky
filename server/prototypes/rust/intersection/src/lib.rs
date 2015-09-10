628ac452182a499f3beaf7c195e462839558423a
Florian R. Hanke <florian.hanke@gmail.com>
2015-09-10 14:52:27 +0200

    Also output asm. Get to naive impl.

--- server/prototypes/rust/intersection/src/lib.rs
+++ server/prototypes/rust/intersection/src/lib.rs
@@ -1,18 +1,36 @@
+use std::collections::HashMap;
+
 trait Intersectable {
-    fn intersect<'a>(&'a self, other: &'a [i16]) -> &[i16]; // <T: FixedSizeArray>
+    fn intersect<'a>(&'a self, other: &'a Vec<i16>) -> Vec<i16>;
 }
 
+// Naïve implementation.
+//
 impl Intersectable for [i16] {
-    fn intersect<'a>(&'a self, other: &'a [i16]) -> &[i16] {
-        other
+    fn intersect<'a>(&'a self, other: &'a Vec<i16>) -> Vec<i16> {
+        let mut result: Vec<i16> = vec![];
+        let mut map = HashMap::new();
+        
+        for item in self.iter() {
+            map.insert(item, item);
+        }
+        
+        for item in other.iter() {
+            match map.get(item) {
+                Some(&res) => result.push(*res),
+                _ => {},
+            }
+        }
+        
+        result
     }
 }
 
 fn main() {
-    let ary1: [i16; 3] = [1,2,3];
-    let ary2: [i16; 3] = [2,3,4];
+    let vec1: Vec<i16> = vec![1,2,3,4,5,6,7,8,9,10];
+    let vec2: Vec<i16> = vec![2,3,4,5,6,7,8,9,10,11];
 
-    let ary3 = ary1.intersect(&ary2);
+    let vec3 = vec1.intersect(&vec2);
     
-    println!("{:?}", ary3);
+    println!("{:?}", vec3);
 }
