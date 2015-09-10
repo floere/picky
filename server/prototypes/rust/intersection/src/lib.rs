c3622f62cf11bd1b79fbaf74cb11c32a49eda55b
Florian R. Hanke <florian.hanke@gmail.com>
2015-09-10 14:36:12 +0200

    General-purpose intersection operation.

---
+++ server/prototypes/rust/intersection/src/lib.rs
@@ -0,0 +1,18 @@
+trait Intersectable {
+    fn intersect(&self) -> &[i16]; // <T: FixedSizeArray>
+}
+
+impl Intersectable for [i16] {
+    fn intersect(&self) -> &[i16] {
+        self
+    }
+}
+
+fn main() {
+    let ary1: [i16; 3] = [1,2,3];
+    // let ary2: [i16; 3] = [2,3,4];
+
+    let ary3 = ary1.intersect();
+    
+    println!("{:?}", ary3);
+}
