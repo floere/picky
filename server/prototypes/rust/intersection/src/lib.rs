6fffc42b42a124e02c1c4453b42b000f33af30a2
Florian R. Hanke <florian.hanke@gmail.com>
2015-09-10 14:40:16 +0200

    Basic scaffold.

--- server/prototypes/rust/intersection/src/lib.rs
+++ server/prototypes/rust/intersection/src/lib.rs
@@ -1,18 +1,18 @@
 trait Intersectable {
-    fn intersect(&self) -> &[i16]; // <T: FixedSizeArray>
+    fn intersect<'a>(&'a self, other: &'a [i16]) -> &[i16]; // <T: FixedSizeArray>
 }
 
 impl Intersectable for [i16] {
-    fn intersect(&self, other: [i16]) -> &[i16] {
-        self
+    fn intersect<'a>(&'a self, other: &'a [i16]) -> &[i16] {
+        other
     }
 }
 
 fn main() {
     let ary1: [i16; 3] = [1,2,3];
-    // let ary2: [i16; 3] = [2,3,4];
+    let ary2: [i16; 3] = [2,3,4];
 
-    let ary3 = ary1.intersect();
+    let ary3 = ary1.intersect(&ary2);
     
     println!("{:?}", ary3);
 }
