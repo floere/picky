f41266e54bb3f91ae042cadda357842b74190441
Florian R. Hanke <florian.hanke@gmail.com>
2015-09-16 18:35:04 +0200

    .

--- server/prototypes/rust/hash_map/src/manual_test.rb
+++ server/prototypes/rust/hash_map/src/manual_test.rb
@@ -1,10 +1,28 @@
 require File.expand_path('../integration', __FILE__)
 
+def mem
+  GC.start
+  puts `ps aux | grep #{Process.pid}`.split("\n").find { |s| s.match(/manual_test/) }
+end
+
 t = Time.now
 
+ary = ('a'..'z').to_a
+
+mem
+
 map = Rust::HashMap.new
-p map.set('hello', 12345)
-p map.get('hello')
-p map.get('hell')
+100_000.times do |i|
+  map.set(ary.shuffle[0..9].join, i)
+end
+
+mem
+
+hash = Hash.new
+100_000.times do |i|
+  hash[ary.shuffle[0..9].join] = i
+end
+
+mem
 
 p Time.now - t
