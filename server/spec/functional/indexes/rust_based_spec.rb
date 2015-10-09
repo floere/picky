6b76eaf42036543bf5d0065e292d0bf0efefe3be
Florian R. Hanke <florian.hanke@gmail.com>
2015-10-10 00:05:52 +0200

    Breakthrough! Picky running Rust indexes!

---
+++ server/spec/functional/indexes/rust_based_spec.rb
@@ -0,0 +1,42 @@
+# encoding: utf-8
+#
+require 'spec_helper'
+
+require File.expand_path '../../../../../indexes/lib/picky-indexes', __FILE__
+
+# Run this test with LD_LIBRARY_PATH=../indexes/lib/picky-indexes/ br spec/functional/indexes/rust_based_spec.rb for now.
+
+describe Rust::Array do
+
+  class Book
+    attr_reader :id, :title, :author
+    def initialize id, title, author
+      @id, @title, @author = id, title, author
+    end
+  end
+
+  attr_reader :data, :books
+
+  let(:data) do
+    Picky::Index.new(:books) do
+      key_format :to_i
+      
+      backend Picky::Backends::Rust.new
+
+      category :title
+      category :author
+    end
+  end
+  let(:books) { Picky::Search.new data }
+
+  before(:each) do
+    data.clear
+    data.add Book.new(1, 'title', 'author')
+    data.add Book.new(2, 'another title', 'another author')
+  end
+
+  it 'searching for it' do
+    expected = Rust::Array.new << 2 << 1
+    books.search('title').ids.should == expected
+  end
+end
