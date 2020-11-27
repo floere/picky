1bde12f00b02335f83b6d135baf9e0c563c56831
Florian R. Hanke <florian.hanke@gmail.com>
2020-11-27 16:05:31 +0100

    Update specs.

--- server/spec/functional/ignore_allocations_spec.rb
+++ server/spec/functional/ignore_allocations_spec.rb
@@ -25,15 +25,15 @@ describe 'ignoring allocations' do
     # These allocations are now removed.
     #
     try.search('some some').allocations.to_result.should == [
-      # [:books, 1.386, 2, [[:text, "some", "some"],   [:text, "some", "some"]],   [2, 1]],
-      [:books, 1.386, 2, [[:text, "some", "some"],   [:title, "some", "some"]],  [2, 1]],
-      [:books, 1.386, 2, [[:title, "some", "some"],  [:text, "some", "some"]],   [2, 1]],
-      [:books, 1.386, 2, [[:title, "some", "some"],  [:title, "some", "some"]],  [2, 1]],
-      [:books, 0.693, 1, [[:title, "some", "some"],  [:author, "some", "some"]], [2]],
-      # [:books, 0.693, 1, [[:author, "some", "some"], [:text, "some", "some"]],   [2]],
-      [:books, 0.693, 1, [[:author, "some", "some"], [:title, "some", "some"]],  [2]],
-      [:books, 0.693, 1, [[:text, "some", "some"],   [:author, "some", "some"]], [2]],
-      [:books, 0.0,   1, [[:author, "some", "some"], [:author, "some", "some"]], [2]]
+      # [:books, 1.386, 2, [[:text, 'some', 'some'],   [:text, 'some', 'some']],   [2, 1]],
+      [:books, 1.386, 2, [[:title, 'some', 'some'],  [:title, 'some', 'some']],  [2, 1]],
+      [:books, 1.386, 2, [[:title, 'some', 'some'],  [:text, 'some', 'some']],   [2, 1]],
+      [:books, 1.386, 2, [[:text, 'some', 'some'],   [:title, 'some', 'some']],  [2, 1]],
+      [:books, 0.693, 1, [[:author, 'some', 'some'], [:title, 'some', 'some']],  [2]],
+      [:books, 0.693, 1, [[:title, 'some', 'some'],  [:author, 'some', 'some']], [2]],
+      # [:books, 0.693, 1, [[:author, 'some', 'some'], [:text, 'some', 'some']],   [2]],
+      [:books, 0.693, 1, [[:text, 'some', 'some'],   [:author, 'some', 'some']], [2]],
+      [:books, 0.0,   1, [[:author, 'some', 'some'], [:author, 'some', 'some']], [2]]
     ]
   end
 end
