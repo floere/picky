6182a4d8a622413f9954bcbcc055ada23a75296f
Florian R. Hanke <florian.hanke@gmail.com>
2020-11-28 16:07:32 +0100

    Switch to PG.

--- server/test_project/data/generate_test_db.sql
+++ server/test_project/data/generate_test_db.sql
@@ -1,25 +1,20 @@
-# MySQL data
-#
-# > mysql -u root
-# CREATE USER developer@localhost;
-# CREATE DATABASE picky_test_project;
-# GRANT ALL ON picky_test_project.* TO developer@localhost;
-#
-# On Florian's local computer:
-# mysql --user developer -D picky_test_project < /Users/hanke/temp/picky/server/test_project/data/generate_test_db.sql
-#
+-- Postgres
+--
+-- > createdb picky_test_project
+-- > psql -D picky_test_project < /Users/admin/temp/picky/server/test_project/data/generate_test_db.sql
+
 DROP TABLE IF EXISTS books;
 
 CREATE TABLE books (
-  id     integer      NOT NULL AUTO_INCREMENT,
-  title  varchar(100) NOT NULL,
-  author varchar(100) NOT NULL,
-  isbn   varchar(10)  NOT NULL,
-  year   decimal(4)   NOT NULL,
-  PRIMARY KEY (id)
+  id        INTEGER,
+  title     TEXT       NOT NULL,
+  author    TEXT       NOT NULL,
+  isbn      TEXT       NOT NULL,
+  year      DECIMAL(4) NOT NULL,
+  publisher TEXT       NOT NULL,
+  category  TEXT       NOT NULL,
+  
+  PRIMARY KEY(id)
 );
 
-LOAD DATA INFILE '/Users/hanke/temp/picky/server/test_project/data/books.csv'
-  INTO TABLE books
-  FIELDS TERMINATED BY ',' ENCLOSED BY '"'
-    LINES TERMINATED BY '\n';
+COPY books (id, title, author, isbn, year, publisher, category) FROM '/Users/admin/temp/picky/server/test_project/data/books.csv' WITH (FORMAT csv, HEADER false);
