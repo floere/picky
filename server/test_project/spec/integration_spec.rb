4f64910d7ced3d826e43f6e5c3660c8dc5186f1e
Florian R. Hanke <florian.hanke@gmail.com>
2020-11-29 12:26:16 +0100

    Update integration specs to new syntax.

--- server/test_project/spec/integration_spec.rb
+++ server/test_project/spec/integration_spec.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 #
 require 'spec_helper'
+require_relative '../app' # Only needed when running this spec alone.
 require_relative '../../../client/lib/picky-client/spec'
 
 describe BookSearch do
@@ -51,11 +52,11 @@ describe BookSearch do
   end
 
   it 'is has the right amount of results' do
-    csv.search('alan').total.should == 3
+    expect(csv.search('alan').total).to eq 3
   end
 
   it 'has correctly structured in-detail results' do
-    csv.search('alan').allocations.should == [
+    expect(csv.search('alan').allocations).to eq [
       ["Books", 6.693, 2, [["author", "alan", "alan"]], [259, 307]],
       ["Books", 0.0,   1, [["title",  "alan", "alan"]], [449]]
     ]
@@ -64,31 +65,31 @@ describe BookSearch do
   # Multicategory search.
   #
   it 'has the right categories' do
-    csv.search('title,author:alan').ids.should == [259, 307, 449]
+    expect(csv.search('title,author:alan').ids).to eq [259, 307, 449]
   end
   it 'simply ignores extraneous categories' do
-    csv.search('title,author,quackquack:alan').ids.should == [259, 307, 449]
+    expect(csv.search('title,author,quackquack:alan').ids).to eq [259, 307, 449]
   end
   it 'will not return anything on wrong categories' do
-    csv.search('quack,moo,derp:alan').ids.should == []
+    expect(csv.search('quack,moo,derp:alan').ids).to eq []
   end
 
   it 'has the right categories' do
-    csv.search('alan').should have_categories(['author'], ['title'])
+    expect(csv.search('alan')).to have_categories(['author'], ['title'])
   end
   it 'has the right categories' do
-    csv.search('a').should have_categories(['author'], ['title'], ['subjects'], ['publisher'])
+    expect(csv.search('a')).to have_categories(['author'], ['title'], ['subjects'], ['publisher'])
   end
 
   it 'finds the same after reloading' do
-    csv.search('soledad human').ids.should == [72]
+    expect(csv.search('soledad human').ids).to eq [72]
     Picky.logger.info "Reloading"
     Picky::Indexes.load
-    csv.search('soledad human').ids.should == [72]
+    expect(csv.search('soledad human').ids).to eq [72]
   end
 
   it 'can handle changing data with a Memory backend' do
-    memory_changing.search('entry').ids.should == [1, 2, 3]
+    expect(memory_changing.search('entry').ids).to eq [1, 2, 3]
 
     new_source = [
       # ChangingItem.new("1", 'first entry'), # Removed.
@@ -100,17 +101,17 @@ describe BookSearch do
     Picky::Indexes[:memory_changing].index
     Picky::Indexes[:memory_changing].load
 
-    memory_changing.search('entry').ids.should == [2, 3, 4]
+    expect(memory_changing.search('entry').ids).to eq [2, 3, 4]
 
     # TODO.
     #
     # Picky::Indexes[:memory_changing].remove 3
     #
-    # memory_changing.search('entry').ids.should == [2, 4]
+    # expect(memory_changing.search('entry').ids).to eq [2, 4]
   end
 
   it 'can handle changing data with a Redis backend' do
-    redis_changing.search('entry').ids.should == ["1", "2", "3"]
+    expect(redis_changing.search('entry').ids).to eq ["1", "2", "3"]
 
     new_source = [
       # ChangingItem.new("1", 'first entry'), # Removed.
@@ -122,13 +123,13 @@ describe BookSearch do
     Picky::Indexes[:redis_changing].index
     Picky::Indexes[:redis_changing].load
 
-    redis_changing.search('entry').ids.should == ["2", "3", "4"]
+    expect(redis_changing.search('entry').ids).to eq ["2", "3", "4"]
 
     # TODO.
     #
     # Picky::Indexes[:redis_changing].remove "3"
     #
-    # redis_changing.search('entry').ids.should == ["2", "4"]
+    # expect(redis_changing.search('entry').ids).to eq ["2", "4"]
   end
 
   # Breakage. As reported by Jason.
@@ -136,67 +137,67 @@ describe BookSearch do
   # TODO Should it find it in [2, 2]?
   #
   it 'finds with specific id' do
-    books.search('id:"2"').ids.should == [2]
+    expect(books.search('id:"2"').ids).to eq [2]
   end
   # # As reported by Simon.
   # #
   # it 'finds a location' do
-  #   @underscore.search('some_place:Zuger some_place:See').should == []
+  #   @underscore.search('some_place:Zuger some_place:See')).to eq []
   # end
 
   # Respects ids param and offset.
   #
-  it { csv.search('title:le* title:hystoree~', :ids => 2, :offset => 0).ids.should == [4, 250] }
-  it { csv.search('title:le* title:hystoree~', :ids => 1, :offset => 1).ids.should == [250] }
+  it { expect(csv.search('title:le* title:hystoree~', :ids => 2, :offset => 0).ids).to eq [4, 250] }
+  it { expect(csv.search('title:le* title:hystoree~', :ids => 1, :offset => 1).ids).to eq [250] }
 
   # Standard tests.
   #
-  it { csv.search('soledad human').ids.should == [72] }
-  it { csv.search('first three minutes weinberg').ids.should == [1] }
+  it { expect(csv.search('soledad human').ids).to eq [72] }
+  it { expect(csv.search('first three minutes weinberg').ids).to eq [1] }
 
   # Standard tests with ignoring unassigned.
   #
-  it { books_ignoring.search('soledad human quack').ids.should == [72] }
-  it { books_ignoring.search('first quack three quack minutes weinberg quack').ids.should == [1] }
+  it { expect(books_ignoring.search('soledad human quack').ids).to eq [72] }
+  it { expect(books_ignoring.search('first quack three quack minutes weinberg quack').ids).to eq [1] }
 
   # "Symbol" keys.
   #
-  it { sym.search('key').ids.should == ['a', 'b', 'c', 'd', 'e', 'f'] }
-  it { sym.search('keydkey').ids.should == ['d'] }
-  it { sym.search('"keydkey"').ids.should == ['d'] }
+  it { expect(sym.search('key').ids).to eq ['a', 'b', 'c', 'd', 'e', 'f'] }
+  it { expect(sym.search('keydkey').ids).to eq ['d'] }
+  it { expect(sym.search('"keydkey"').ids).to eq ['d'] }
 
   # Complex cases.
   #
-  it { csv.search('title:le* title:hystoree~').ids.should == [4, 250, 428] }
-  it { csv.search('hystori~ author:ferg').ids.should == [] }
-  it { csv.search('hystori~ author:fergu').ids.should == [4] }
-  it { csv.search('hystori~ author:fergus').ids.should == [4] }
-  it { csv.search('author:fergus').ids.should == [4] }
+  it { expect(csv.search('title:le* title:hystoree~').ids).to eq [4, 250, 428] }
+  it { expect(csv.search('hystori~ author:ferg').ids).to eq [] }
+  it { expect(csv.search('hystori~ author:fergu').ids).to eq [4] }
+  it { expect(csv.search('hystori~ author:fergus').ids).to eq [4] }
+  it { expect(csv.search('author:fergus').ids).to eq [4] }
 
   # Partial searches.
   #
-  it { csv.search('gover* systems').ids.should == [7] }
-  it { csv.search('a*').ids.should == [4, 7, 8, 80, 117, 119, 125, 132, 168, 176, 184, 222, 239, 242, 333, 346, 352, 361, 364, 380] }
-  it { csv.search('a* b* c* d* f').ids.should == [110, 416] }
-  it { csv.search('1977').ids.should == [86, 394] }
+  it { expect(csv.search('gover* systems').ids).to eq [7] }
+  it { expect(csv.search('a*').ids).to eq [4, 7, 8, 80, 117, 119, 125, 132, 168, 176, 184, 222, 239, 242, 333, 346, 352, 361, 364, 380] }
+  it { expect(csv.search('a* b* c* d* f').ids).to eq [110, 416] }
+  it { expect(csv.search('1977').ids).to eq [86, 394] }
 
   # Similarity.
   #
-  it { csv.search('hystori~ leeward').ids.should == [4] }
-  it { csv.search('strutigic~ guvurnance~').ids.should == [7] }
-  it { csv.search('strategic~ governance~').ids.should == [] } # Does not find itself.
+  it { expect(csv.search('hystori~ leeward').ids).to eq [4] }
+  it { expect(csv.search('strutigic~ guvurnance~').ids).to eq [7] }
+  it { expect(csv.search('strategic~ governance~').ids).to eq [] } # Does not find itself.
 
   # Qualifiers.
   #
-  it { csv.search('title:history author:fergus').ids.should == [4] }
+  it { expect(csv.search('title:history author:fergus').ids).to eq [4] }
 
   # Splitting.
   #
-  it { csv.search('history/fergus&history/history&fergus').ids.should == [4, 4, 4, 4, 4, 4, 4, 4] }
+  it { expect(csv.search('history/fergus&history/history&fergus').ids).to eq [4, 4, 4, 4, 4, 4, 4, 4] }
 
   # Character Removal.
   #
-  it { csv.search("'(history)' '(fergus)'").ids.should == [4, 4] }
+  it { expect(csv.search("'(history)' '(fergus)'").ids).to eq [4, 4] }
 
   # Contraction.
   #
@@ -204,149 +205,149 @@ describe BookSearch do
 
   # Stopwords.
   #
-  it { csv.search("and the history or fergus").ids.should == [4, 4] }
-  it { csv.search("and the or on of in is to from as at an history fergus").ids.should == [4, 4] }
+  it { expect(csv.search("and the history or fergus").ids).to eq [4, 4] }
+  it { expect(csv.search("and the or on of in is to from as at an history fergus").ids).to eq [4, 4] }
 
   # Normalization.
   #
-  it { csv.search('Deoxyribonucleic Acid').ids.should == [] }
-  it { csv.search('800 dollars').ids.should == [] }
+  it { expect(csv.search('Deoxyribonucleic Acid').ids).to eq [] }
+  it { expect(csv.search('800 dollars').ids).to eq [] }
 
   # Remove after splitting.
   #
-  it { csv.search("his|tory fer|gus").ids.should == [4, 4] }
+  it { expect(csv.search("his|tory fer|gus").ids).to eq [4, 4] }
 
   # Character Substitution.
   #
-  it { csv.search("hïstôry educåtioñ fërgus").ids.should == [4, 4, 4, 4] }
+  it { expect(csv.search("hïstôry educåtioñ fërgus").ids).to eq [4, 4, 4, 4] }
 
   # Token Rejection.
   #
-  it { csv.search('amistad').ids.should == [] }
+  it { expect(csv.search('amistad').ids).to eq [] }
 
   # Breakage.
   #
-  it { csv.search("%@{*^$!*$$^!&%!@%#!%#(#!@%#!#!)}").ids.should == [] }
-  it { csv.search("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").ids.should == [] }
-  it { csv.search("glorfgnorfblorf").ids.should == [] }
+  it { expect(csv.search("%@{*^$!*$$^!&%!@%#!%#(#!@%#!#!)}").ids).to eq [] }
+  it { expect(csv.search("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").ids).to eq [] }
+  it { expect(csv.search("glorfgnorfblorf").ids).to eq [] }
 
   # Range based area search. Memory.
   #
-  it { simple_geo.search("north1:47.41 east1:8.55").ids.should == [1481, 5014, 5015, 5016, 10576, 10717, 17777, 17999] }
+  it { expect(simple_geo.search("north1:47.41 east1:8.55").ids).to eq [1481, 5014, 5015, 5016, 10576, 10717, 17777, 17999] }
 
   # Geo based area search.
   #
-  it { geo.search("north:47.41 east:8.55").ids.should == [1481, 5010, 5011, 5012, 5013, 5014, 5015, 5016, 5017, 5059, 9110, 10347, 10576, 10717, 10879, 17777, 17955, 18038] }
+  it { expect(geo.search("north:47.41 east:8.55").ids).to eq [1481, 5010, 5011, 5012, 5013, 5014, 5015, 5016, 5017, 5059, 9110, 10347, 10576, 10717, 10879, 17777, 17955, 18038] }
 
   # Redis.
   #
-  it { redis.search('soledad human').ids.should == ['72'] }
-  it { redis.search('first three minutes weinberg').ids.should == ['1'] }
-  it { redis.search('gover* systems').ids.should == ['7'] }
-  it { redis.search('a*').ids.should == ['4', '7', '8', '80', '117', '119', '125', '132', '168', '176', '184', '222', '239', '242', '333', '346', '352', '361', '364', '380'] }
-  it { redis.search('a* b* c* d* f').ids.should == ['110', '416'] }
-  it { redis.search('1977').ids.should == ['86', '394'] }
+  it { expect(redis.search('soledad human').ids).to eq ['72'] }
+  it { expect(redis.search('first three minutes weinberg').ids).to eq ['1'] }
+  it { expect(redis.search('gover* systems').ids).to eq ['7'] }
+  it { expect(redis.search('a*').ids).to eq ['4', '7', '8', '80', '117', '119', '125', '132', '168', '176', '184', '222', '239', '242', '333', '346', '352', '361', '364', '380'] }
+  it { expect(redis.search('a* b* c* d* f').ids).to eq ['110', '416'] }
+  it { expect(redis.search('1977').ids).to eq ['86', '394'] }
 
   # Categorization.
   #
-  it { csv.search('t:religion').ids.should == csv.search('title:religion').ids }
-  it { csv.search('title:religion').ids.should_not == csv.search('subject:religion').ids }
+  it { expect(csv.search('t:religion').ids).to eq csv.search('title:religion').ids }
+  it { expect(csv.search('title:religion').ids).to_not eq csv.search('subject:religion').ids }
 
   # Wrong categorization.
   #
   # From 2.5.0 on, Picky does not remove wrong categories anymore. Wrong categories return zero results.
   #
-  it { csv.search('gurk:religion').ids.should == [] }
+  it { expect(csv.search('gurk:religion').ids).to eq [] }
 
   # Index-specific tokenizer.
   #
   it 'does not find abc' do
-    indexing.search('human perception').ids.should == []
+    expect(indexing.search('human perception').ids).to eq []
   end
   it 'does find without a or b or c' do
-    indexing.search('humn pereption').ids.should == [72]
+    expect(indexing.search('humn pereption').ids).to eq [72]
   end
 
   # Boosting.
   #
   # it 'boosts the right combination' do
-  #   csv.search('history').ids.should == [4, 5, 6, 7, 10, 16, 21, 23, 24, 37, 40, 43, 47, 53, 55, 59, 68, 76, 85, 90]
+  #   expect(csv.search('history').ids).to eq [4, 5, 6, 7, 10, 16, 21, 23, 24, 37, 40, 43, 47, 53, 55, 59, 68, 76, 85, 90]
   # end
 
   # Downcasing.
   #
-  it { csv.search("history fergus").ids.should == [4, 4] }
-  it { csv.search("HISTORY FERGUS").ids.should == [] }
-  it { csv.search("history AND OR fergus").ids.should == [4, 4] }
+  it { expect(csv.search("history fergus").ids).to eq [4, 4] }
+  it { expect(csv.search("HISTORY FERGUS").ids).to eq [] }
+  it { expect(csv.search("history AND OR fergus").ids).to eq [4, 4] }
 
   # File based.
   #
-  it { file.search("first").ids.should == [1] }
-  it { file.search("entry").ids.should == [1,2,3] }
-  it { file.search("entry first").ids.should == [1] }
+  it { expect(file.search("first").ids).to eq [1] }
+  it { expect(file.search("entry").ids).to eq [1,2,3] }
+  it { expect(file.search("entry first").ids).to eq [1] }
 
   # Infix partial.
   #
-  it { file.search("ntr* rst*").ids.should == [1] }
+  it { expect(file.search("ntr* rst*").ids).to eq [1] }
 
   # Japanese characters (UTF-8).
   #
-  it { japanese.search("日本語").ids.should == [1] }
-  it { japanese.search("にほんご").ids.should == [1] }
-  it { japanese.search("食べる").ids.should == [2] }
-  it { japanese.search("たべる").ids.should == [2] }
+  it { expect(japanese.search("日本語").ids).to eq [1] }
+  it { expect(japanese.search("にほんご").ids).to eq [1] }
+  it { expect(japanese.search("食べる").ids).to eq [2] }
+  it { expect(japanese.search("たべる").ids).to eq [2] }
   #
   # Partial.
   #
-  it { japanese.search("日").ids.should == [1] }
+  it { expect(japanese.search("日").ids).to eq [1] }
 
   # Different tokenizer.
   #
-  it { nonstring.search("moo zap").ids.should == [2] }
+  it { expect(nonstring.search("moo zap").ids).to eq [2] }
 
   # Partial options.
   #
-  it { partial.search("substring:oct").ids.should == [] }
-  it { partial.search("substring:octo").ids.should == [] }
-  it { partial.search("substring:octop").ids.should == [1] }
-  it { partial.search("substring:octopu").ids.should == [1] }
-  it { partial.search("substring:octopus").ids.should == [1] }
-  it { partial.search("substring:octopuss").ids.should == [] }
-  it { partial.search("substring:octopussy").ids.should == [] }
+  it { expect(partial.search("substring:oct").ids).to eq [] }
+  it { expect(partial.search("substring:octo").ids).to eq [] }
+  it { expect(partial.search("substring:octop").ids).to eq [1] }
+  it { expect(partial.search("substring:octopu").ids).to eq [1] }
+  it { expect(partial.search("substring:octopus").ids).to eq [1] }
+  it { expect(partial.search("substring:octopuss").ids).to eq [] }
+  it { expect(partial.search("substring:octopussy").ids).to eq [] }
 
-  it { partial.search("postfix:oct").ids.should == [] }
-  it { partial.search("postfix:octo").ids.should == [] }
-  it { partial.search("postfix:octop").ids.should == [1] }
-  it { partial.search("postfix:octopu").ids.should == [1] }
-  it { partial.search("postfix:octopus").ids.should == [1] }
-  it { partial.search("postfix:octopuss").ids.should == [1] }
-  it { partial.search("postfix:octopussy").ids.should == [1] }
+  it { expect(partial.search("postfix:oct").ids).to eq [] }
+  it { expect(partial.search("postfix:octo").ids).to eq [] }
+  it { expect(partial.search("postfix:octop").ids).to eq [1] }
+  it { expect(partial.search("postfix:octopu").ids).to eq [1] }
+  it { expect(partial.search("postfix:octopus").ids).to eq [1] }
+  it { expect(partial.search("postfix:octopuss").ids).to eq [1] }
+  it { expect(partial.search("postfix:octopussy").ids).to eq [1] }
 
-  it { partial.search("infix:c").ids.should == [1,2] }
-  it { partial.search("infix:br").ids.should == [2] }
-  it { partial.search("infix:cad").ids.should == [2] }
+  it { expect(partial.search("infix:c").ids).to eq [1,2] }
+  it { expect(partial.search("infix:br").ids).to eq [2] }
+  it { expect(partial.search("infix:cad").ids).to eq [2] }
 
-  it { partial.search("none:octopussy").ids.should == [1] }
-  it { partial.search("none:abracadabra").ids.should == [2] }
+  it { expect(partial.search("none:octopussy").ids).to eq [1] }
+  it { expect(partial.search("none:abracadabra").ids).to eq [2] }
 
   # Search#ignore option.
   #
   # TODO Removed in 4.9+. Reintroduce!
   #
-  it { book_each.search("alan history").ids.should == ["259", "307"] } # Ignores History or Alan in title.
+  it { expect(book_each.search("alan history").ids).to eq ["259", "307"] } # Ignores History or Alan in title.
 
   # SQLite backend.
   #
-  it { sqlite.search("hello sqlite").ids.should == [1] }
+  it { expect(sqlite.search("hello sqlite").ids).to eq [1] }
   
   # Commas in ids.
   #
-  it { commas.search("text:with").ids.should == ['a,b', 'c,d'] }
+  it { expect(commas.search("text:with").ids).to eq ['a,b', 'c,d'] }
 
   # Database index reloading.
   #
   it 'can handle changing data with a Memory backend with a non-each DB source' do
-    # books.search('1977').ids.should == [86, 394]
+    # expect(books.search('1977').ids).to eq [86, 394]
 
     # Some webapp adds a book.
     #
@@ -363,13 +364,13 @@ describe BookSearch do
     #
     added_book.destroy
 
-    books.search('1977').ids.should == [86, 394, expected_id]
+    expect(books.search('1977').ids).to eq [86, 394, expected_id]
   end
 
   # Database index reloading.
   #
   it 'can handle changing data with a Memory backend with an each DB source' do
-    book_each.search('1977').ids.should == ["86", "394"]
+    expect(book_each.search('1977').ids).to eq ["86", "394"]
 
     # Some webapp adds a book.
     #
@@ -386,7 +387,7 @@ describe BookSearch do
     #
     added_book.destroy
 
-    book_each.search('1977').ids.should == ["86", "394", expected_id.to_s]
+    expect(book_each.search('1977').ids).to eq ["86", "394", expected_id.to_s]
   end
 
 end
