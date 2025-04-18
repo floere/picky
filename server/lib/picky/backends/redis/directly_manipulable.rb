1069ecddb6cfb14670f3bad8f19653166fa70bd1
Florian R. Hanke <florian.hanke@gmail.com>
2025-04-18 21:06:05 +0200

    .

--- server/lib/picky/backends/redis/directly_manipulable.rb
+++ server/lib/picky/backends/redis/directly_manipulable.rb
@@ -2,6 +2,12 @@ module Picky
   module Backends
     class Redis
       module DirectlyManipulable
+        mattr_accessor :append_index
+        self.append_index = 0
+
+        mattr_accessor :unshift_index
+        self.unshift_index = 0
+
         attr_accessor :backend, :key
 
         def self.make(backend, list, key)
@@ -12,18 +18,16 @@ module Picky
 
         # THINK Current implementation is very brittle.
         #
-        @@append_index = 0
         def <<(value)
           super
-          zadd value, @@append_index += 1
+          zadd value, DirectlyManipulable.append_index += 1
         end
 
         # THINK Current implementation is very brittle.
         #
-        @@unshift_index = 0
         def unshift(value)
           super
-          zadd value, @@unshift_index -= 1
+          zadd value, DirectlyManipulable.unshift_index -= 1
         end
 
         # Deletes the value.
